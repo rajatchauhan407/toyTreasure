da992126e7799fdd4efa18269b69af6193e12459
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-05-28 12:23:29 -0700

    React Basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/button';
+import Header from './header';
 
 function App() {
   return (
     <div className="App">
-      
+    <Header/>
+    <Button>
+        Donate Now
+    </Button>
     </div>
   );
 }
