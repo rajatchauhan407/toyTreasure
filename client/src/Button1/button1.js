4ffbe7e3e46c9f7c8e2c35c1dcda468299bbb0b4
MagdelineJannet <janet.magdeline@gmail.com>
2023-05-28 13:22:57 -0700

    Button1

---
+++ client/src/Button1/button1.js
@@ -0,0 +1,9 @@
+import './button1.css';
+export default function Button1(props){
+   let button1="Hello"
+    return(
+        <button className="Button1">
+           {button1}
+        </button>
+    );
+}
