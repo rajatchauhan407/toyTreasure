d729d49b32064f408f5f869622589878ca49f2cc
cindycharvet <cindycharvetweb@gmail.com>
2023-05-28 12:39:19 -0700

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Vali from './validate';
 import {Greeting} from './validate';
+import Button from './components/button';
 
 function App() {
   Vali();
   Greeting();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>
+        Verify Donation
+      </Button>
+      
     </div>
   );
 }
