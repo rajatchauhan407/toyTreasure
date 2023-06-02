601808aab2f65509dceeae8edd3ef93bd98118fd
MagdelineJannet <janet.magdeline@gmail.com>
2023-06-02 12:35:30 -0700

    Cards & Categories

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import B1 from './Button1/button1';
+import Card from './Card';
+import Categories from './Categories';
 
 function App() {
   return (
     <div className="App">
    <B1/>
+   
+   <Categories></Categories>
     </div>
   );
 }
