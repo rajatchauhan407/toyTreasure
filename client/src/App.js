0c0c3e0fc8bf424648d14562acbc6461004e5ad4
MagdelineJannet <janet.magdeline@gmail.com>
2023-06-02 11:15:40 -0700

    modified app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from './components/button';
-import Header from './header';
 import B1 from './Button1/button1';
 
 function App() {
-  Validate();
-  greeting();
   return (
     <div className="App">
    <B1/>
