d845c1e327b071797567887083ebb2f8f73527c6
MagdelineJannet <janet.magdeline@gmail.com>
2023-06-10 20:40:34 -0700

    Aligned code for side menu

--- client/src/components/side-menu/index.js
+++ client/src/components/side-menu/index.js
@@ -4,26 +4,22 @@ import logo from "./appLogo.svg"
 export default function OrgSideMenu(props)
 {
     return(
-        <div className="OrgSideMenuWrapper">
-           
-           <img className="site-logo" src={logo} alt={"siteLogo"}/>  
-           
-                <nav className="main-menu">
-                    <ul>
-                        <li><a href="#"><i className="fa fa-user-o" aria-hidden="true"></i> Profile</a></li>
-                        <li><a href="#"><i className="fa-sharp fa-solid fa-chart-line"></i> Dashboard</a></li>               
-                        <li><a href="#"><i className="fa-brands fa-wpforms"></i> Verification</a></li>
-                        <li><a href="#"><i className="fa fa-cog" aria-hidden="true"></i> Setting</a></li>                
-                    </ul>
-                </nav>   
-
-                <nav className="contact-menu">
-                    <ul>
-                        <li><a href="#"><i className="fa fa-commenting-o" aria-hidden="true"></i> Contact Us</a></li>
-                        <li><a href="#"><i className="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>               
-                    </ul>
-                </nav>  
-             
+        <div className="OrgSideMenuWrapper">           
+           <img className="site-logo" src={logo} alt={"siteLogo"}/>           
+            <nav className="main-menu">
+                <ul>
+                    <li><a href="#"><i className="fa fa-user-o" aria-hidden="true"></i> Profile</a></li>
+                    <li><a href="#"><i className="fa-sharp fa-solid fa-chart-line"></i> Dashboard</a></li>               
+                    <li><a href="#"><i className="fa-brands fa-wpforms"></i> Verification</a></li>
+                    <li><a href="#"><i className="fa fa-cog" aria-hidden="true"></i> Setting</a></li>                
+                </ul>
+            </nav>
+            <nav className="contact-menu">
+                <ul>
+                    <li><a href="#"><i className="fa fa-commenting-o" aria-hidden="true"></i> Contact Us</a></li>
+                    <li><a href="#"><i className="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>               
+                </ul>
+            </nav>             
         </div>
     )
 }
