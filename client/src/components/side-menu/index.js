7ea9015923b48ffc8430c50b8714e054cdc7afe6
MagdelineJannet <janet.magdeline@gmail.com>
2023-06-10 20:24:18 -0700

    side menu updated

--- client/src/components/side-menu/index.js
+++ client/src/components/side-menu/index.js
@@ -1,24 +1,26 @@
 import "./index.css";
+import logo from "./appLogo.svg"
 
 export default function OrgSideMenu(props)
 {
     return(
         <div className="OrgSideMenuWrapper">
-           <img className="site-logo" src="./images/logo-ee-8.png" alt="siteLogo"/>       
+           
+           <img className="site-logo" src={logo} alt={"siteLogo"}/>  
            
                 <nav className="main-menu">
                     <ul>
-                        <li><a href="#"><i class="fa fa-user-o" aria-hidden="true"></i> Profile</a></li>
-                        <li><a href="#"><i class="fa-sharp fa-solid fa-chart-line"></i> Dashboard</a></li>               
-                        <li><a href="#"><i class="fa-brands fa-wpforms"></i> Verification</a></li>
-                        <li><a href="#"><i class="fa fa-cog" aria-hidden="true"></i> Setting</a></li>                
+                        <li><a href="#"><i className="fa fa-user-o" aria-hidden="true"></i> Profile</a></li>
+                        <li><a href="#"><i className="fa-sharp fa-solid fa-chart-line"></i> Dashboard</a></li>               
+                        <li><a href="#"><i className="fa-brands fa-wpforms"></i> Verification</a></li>
+                        <li><a href="#"><i className="fa fa-cog" aria-hidden="true"></i> Setting</a></li>                
                     </ul>
                 </nav>   
 
                 <nav className="contact-menu">
                     <ul>
-                        <li><a href="#"><i class="fa fa-commenting-o" aria-hidden="true"></i> Contact Us</a></li>
-                        <li><a href="#"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>               
+                        <li><a href="#"><i className="fa fa-commenting-o" aria-hidden="true"></i> Contact Us</a></li>
+                        <li><a href="#"><i className="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>               
                     </ul>
                 </nav>  
              
