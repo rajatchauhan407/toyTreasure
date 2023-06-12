766aa723cd4154de03bf25f9b39945d52b404560
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-06-12 14:15:45 -0700

    changes made regarding CI/CD

--- client/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = function override(config) {
-  // Modify the output path
-  config.output.path = path.join(__dirname, '../build');
-
-  return config;
-};
