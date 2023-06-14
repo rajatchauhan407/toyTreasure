a167d5aff37ff322ad34329c1ddb142eb64ea3b9
Raha <r.emami98@gmail.com>
2023-06-13 17:42:44 -0700

    Part 2

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,27 @@ import './App.css';
 import Card from './components/card-vertification-pending-donor';
 
 function App() {
-  return (
-    
-  );
-}
+  const verification = [
+    {orderNumber: 37890, name:"Greg Thomas", method: "drop-off", date: "05/05/2023", Qty: 6, donationStatus: "Pending"},
+    {orderNumber: 37890, name:"Greg Thomas", method: "drop-off", date: "05/05/2023", Qty: 6, donationStatus: "Pending"},
+    {orderNumber: 37890, name:"Greg Thomas", method: "drop-off", date: "05/05/2023", Qty: 6, donationStatus: "Pending"},
+]
 
+    <div className="Verification">
+      {verification.map((el)=>{
+                return <Card ={el.orderNumber} age={el.age}/>
+            })}
+      <Categories>
+        <Button>Donate Now</Button>
+      </Categories>
+      {
+        //  userIsLoggedIn?<Rewards/>:""
+      }
+      {
+        users.map((el)=>{
+         return userIsLoggedIn?(<Rewards
+          name={el.name}
+          rewardPoint={el.rewardPoints}
+          />):""
+        });
 export default App;
