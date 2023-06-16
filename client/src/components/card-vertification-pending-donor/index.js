089f0bfd1e4cf24e0d877142f97f96a442218aed
Raha <r.emami98@gmail.com>
2023-06-16 11:08:41 -0700

    I don't know the changes but let's commit

--- client/src/components/card-vertification-pending-donor/index.js
+++ client/src/components/card-vertification-pending-donor/index.js
@@ -5,7 +5,6 @@ export default function Card(props){
             <div className="orderNumber"><li>{props.orderNumber}</li></div>
             <div className="listInfo">
                 <ul>
-                    
                     <li>{props.method}</li>
                     <li>{props.date}</li>
                     <li>{props.Qty}</li>
