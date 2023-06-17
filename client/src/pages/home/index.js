235ba36d4cc0ba8375e16eccc4a3d15a07e0c6ac
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-06-17 14:15:35 -0700

    firebase-auth

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,7 +1,14 @@
 import './index.css';
-
+import {useState} from "react";
+import FireBaseAuthService from '../../FirebaseAuthService';
+import LoginForm from '../../components/login'
 export default function Home(){
+    const [user, setUser] = useState(null);
+  FireBaseAuthService.subscribeToAuthChanges(setUser);
     return(<>
             <h1>Home</h1>
+            <LoginForm 
+          existingUser={user}
+        />
     </>);
 }
