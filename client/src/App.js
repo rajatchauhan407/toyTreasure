99faba74f3e444788bf4d310371cef062e100c63
Raha <r.emami98@gmail.com>
2023-06-16 19:59:27 -0700

    nothing is changed but let's commit it

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,19 @@ import firebase from './FirebaseConfig';
 
 import Routing from "./routes/routing";
 function App() {
+  let promise = new Promise((res, rej)=>{
+let array = ["Cindy","Raha","Jannet"];
+res(array);
+
+  })
+  promise.then((data)=>{
+console.log(data);
+  });
+  console.log ("Hello World");
+  setTimeout(()=> {
+    console.log("Inside another world");
+  },1000);
+  console.log("Hello word 2")
   return (
     <div className="App">
         <Routing/>
