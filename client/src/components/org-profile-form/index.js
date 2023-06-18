3a90147e6d8d6d97f88329232b18eb9b53d8edb6
cindycharvet <cindycharvetweb@gmail.com>
2023-06-18 00:01:24 -0700

    Org Profile Style Done

--- client/src/components/org-profile-form/index.js
+++ client/src/components/org-profile-form/index.js
@@ -1,4 +1,5 @@
 import MultiSelector from "../multi-selector";
+import "./index.css"
 
 export default function OrgProfileForm() {
 
@@ -18,37 +19,40 @@ return (
                 <input type="text" id="orgCRA" name="orgCRA" pattern="[a-zA-Z0-9]{1,15}" required /><br />
     
                 <div className="one-line">
-                    <label htmlFor="orgAddress1">Address Line 1</label><br />
-                    <input type="text" id="orgAddress1" name="orgAddress1" required /><br />
-        
-                    <label htmlFor="orgAddress2">Address Line 2</label><br />
-                    <input type="text" id="orgAddress2" name="orgAddress2" required /><br />
-        
-                    <label htmlFor="orgCity">City</label><br />
-                    <input type="text" id="orgCity" name="orgCity" required /><br />
-        
-                    <label htmlFor="orgZip">Zip Code</label><br />
-                    <input type="text" id="orgZip" name="orgZip" required /><br />
-        
-                    <label htmlFor="orgProvince">Province</label><br />
-                    <select id="orgProvince" name="orgProvince" required>
-                        <option value="Alberta">Alberta</option>
-                        <option value="British Columbia">British Columbia</option>
-                        <option value="Manitoba">Manitoba</option>
-                        <option value="New Brunswick">New Brunswick</option>
-                        <option value="Newfoundland and Labrador">Newfoundland and Labrador</option>
-                        <option value="Northwest Territories">Northwest Territories</option>
-                        <option value="Nova Scotia">Nova Scotia</option>
-                        <option value="Nunavut">Nunavut</option>
-                        <option value="Ontario">Ontario</option>
-                        <option value="Prince Edward Island">Prince Edward Island</option>
-                        <option value="Quebec">Quebec</option>
-                        <option value="Saskatchewan">Saskatchewan</option>
-                        <option value="Yukon">Yukon</option>
-                    </select><br />
-        
-                    <label htmlFor="orgCountry">Country</label><br />
-                    <input type="text" id="orgCountry" name="orgCountry" value="Canada" disabled /><br />
+                    <div className="sect-1">
+                        <label htmlFor="orgAddress1">Address Line 1</label><br />
+                        <input type="text" id="orgAddress1" name="orgAddress1" required /><br />
+
+                        <label htmlFor="orgCity">City</label><br />
+                        <input type="text" id="orgCity" name="orgCity" required /><br />
+
+                        <label htmlFor="orgProvince">Province</label><br />
+                        <select id="orgProvince" name="orgProvince" required>
+                            <option value="Alberta">Alberta</option>
+                            <option value="British Columbia">British Columbia</option>
+                            <option value="Manitoba">Manitoba</option>
+                            <option value="New Brunswick">New Brunswick</option>
+                            <option value="Newfoundland and Labrador">Newfoundland and Labrador</option>
+                            <option value="Northwest Territories">Northwest Territories</option>
+                            <option value="Nova Scotia">Nova Scotia</option>
+                            <option value="Nunavut">Nunavut</option>
+                            <option value="Ontario">Ontario</option>
+                            <option value="Prince Edward Island">Prince Edward Island</option>
+                            <option value="Quebec">Quebec</option>
+                            <option value="Saskatchewan">Saskatchewan</option>
+                            <option value="Yukon">Yukon</option>
+                        </select><br />
+                    </div>
+                    <div className="sect-2">
+                        <label htmlFor="orgAddress2">Address Line 2</label><br />
+                        <input type="text" id="orgAddress2" name="orgAddress2" required /><br />
+            
+                        <label htmlFor="orgZip">Zip Code</label><br />
+                        <input type="text" id="orgZip" name="orgZip" required /><br />
+            
+                        <label htmlFor="orgCountry">Country</label><br />
+                        <input type="text" id="orgCountry" name="orgCountry" value="Canada" disabled /><br />
+                    </div>
                 </div>
         
                 <label htmlFor="orgEmail">Email</label><br />
@@ -63,30 +67,37 @@ return (
         
             <form className= "Impact">
                 <h2>Impact of Organization</h2>
-                
-                <div className="Section1">
-                    <label htmlFor="yearFounded">Year Founded</label> <br />
-                    <input type="number" id="yearFounded" name="yearFounded" min="1900" max="2023" required /> <br />
+                <p>Add the numbers of impact in your organization</p>
 
-                    <label htmlFor="numDonors">Number of Donors</label> <br />
-                    <input type="number" id="numDonors" name="numDonors" min="0" required /> <br />
-                </div>
+                <div className="statistics">
+                    <div className="Section1">
+                        <label htmlFor="yearFounded">Year Founded</label> <br />
+                        <input type="number" id="yearFounded" name="yearFounded" min="1900" max="2023" required /> <br />
 
-                <div className="Section2">
-                    <label htmlFor="numToysReceived">Number of Toys Received</label><br />
-                    <input type="number" id="numToysReceived" name="numToysReceived" min="0" required /> <br />
+                        <label htmlFor="numDonors">Donors</label> <br />
+                        <input type="number" id="numDonors" name="numDonors" min="0" required /> <br />
 
-                    <label htmlFor="numKidsSupported">Number of Kids Being Supported</label><br />
-                    <input type="number" id="numKidsSupported" name="numKidsSupported" min="0" required /> <br />
+                        <label htmlFor="autoUpdate">Auto-update Numbers</label>
+                        <input type="checkbox" id="autoUpdate" name="autoUpdate" />
+                    </div>
 
-                    <label htmlFor="autoUpdate">Auto-update Numbers</label>
-                    <input type="checkbox" id="autoUpdate" name="autoUpdate" />
+                    <div className="Section2">
+                        <label htmlFor="numToysReceived">Toys Received</label><br />
+                        <input type="number" id="numToysReceived" name="numToysReceived" min="0" required /> <br />
+
+                        <label htmlFor="numKidsSupported">Kids Being Supported</label><br />
+                        <input type="number" id="numKidsSupported" name="numKidsSupported" min="0" required /> <br />
+                    </div>
                 </div>
             </form>
+
+            <button type="submit">Save</button>
+            <button type="reset">Reset</button>
         </div>
+
         {/* RIGHT COLUMN ================ */}
         <div className="wrapper-right">
-            <div className="Upload">
+            <form className="Upload">
                 <label htmlFor="logoUpload">Upload Logo</label><br />
                 <input type="file" id="logoUpload" name="logoUpload" accept="image/*" className="logo-input" required/><br />       
 
@@ -95,22 +106,20 @@ return (
 
                 <label htmlFor="videoUpload">Upload Video</label><br />
                 <input type="file" id="videoUpload" name="videoUpload" accept="video/*" className="video-input" required/><br />      
-            </div>
+            </form>
 
-            <div className="ExtraDetails">
-                <div className="details">
-                    <h2>Additional Details</h2>
-                    <label htmlFor="deliveryMethod">Preferred Donation Delivery Method</label><br />
+        
+            <form className="ExtraDetails">
+                <h2>Additional Details</h2>
+                <label htmlFor="deliveryMethod">Donation Delivery Method</label><br />
 
-                    <label htmlFor="avaliableTime">Avaliable Time</label><br />
+                <label htmlFor="avaliableTime">Avaliable Time</label><br />
 
-                    <label htmlFor="avaliableDays">Avaliable Days</label><br />
-                    {/* <MultiSelector /> */}
-                </div>
-            </div>
+                <label htmlFor="avaliableDays">Avaliable Days</label><br />
+                {/* <MultiSelector /> */}
+            </form>
 
-            <div className="Categories">
-                <div className="KidsList">
+            <form className="Categories">
                     <h2>Select the categories of toys you accept</h2>
                     <div className="Kids">
                         <h3>Kids Toys</h3>
@@ -168,13 +177,8 @@ return (
                             <input type="checkbox" id="sensoryToys" name="category" value="Sensory Balls and Textured Toys"/>
                             <label for="sensoryToys">Sensory Balls and Textured Toys</label><br />
                     </div>
-                </div>
-            </div>
+            </form>
         </div>
-        
-        <button type="submit">Save</button>
-        <button type="reset">Reset</button>
-
     </div>
 );
 } 
