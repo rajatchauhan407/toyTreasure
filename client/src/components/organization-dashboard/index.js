bf6d55afd85651775ec95ed9c921f3b7d388dfde
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-06-19 04:01:08 -0700

    completed dashboard... Refactoring Needed

---
+++ client/src/components/organization-dashboard/index.js
@@ -0,0 +1,214 @@
+import './index.scss';
+import {Link} from "react-router-dom";
+
+export default function OrgDashBoard(){
+    return (
+        <div className='org-wrapper'>
+            <div className="org-list-wrapper">
+            <div className="org-list-header">
+                <h2>DashBoard</h2>
+                <button>
+                    <i className="fa-solid fa-plus"></i>&nbsp;&nbsp;Add to Wishlist
+                </button>
+            </div>
+            <h3>Wishlist</h3>
+            <div className="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Tag Category</td>
+                            <td>Qty Required</td>
+                            <td>Qty Received</td>
+                            <td>Pending</td>
+                            <td>Icon</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div className="side-container">
+            <div className="cards-wrapper">
+                <div className="card-wrapper">
+                    <div className="img-wrapper">
+                        <img src="https://picsum.photos/400/400?rand=431" alt="randompic"/>
+                    </div>
+                    <p>
+                        256
+                    </p>
+                    <p>
+                        Total Donors
+                    </p>
+               </div> 
+               <div className="card-wrapper">
+                    <div className="img-wrapper">
+                        <img src="https://picsum.photos/400/400?rand=433" alt="randompic"/>
+                    </div>
+                    <p>
+                        256
+                    </p>
+                    <p>
+                        Total Donors
+                    </p>
+               </div> 
+               <div className="card-wrapper">
+                    <div className="img-wrapper">
+                        <img src="https://picsum.photos/400/400?rand=434" alt="randompic"/>
+                    </div>
+                    <p>
+                        256
+                    </p>
+                    <p>
+                        Total Donors
+                    </p>
+               </div> 
+               <div className="card-wrapper">
+                <div className="img-wrapper">
+                    <img src="https://picsum.photos/400/400?rand=435" alt="randompic"/>
+                </div>
+                <p>
+                    256
+                </p>
+                <p>
+                    Total Donors
+                </p>
+            </div>       
+           </div> 
+           <div className="org-pending-verification">
+                <div className="org-pending-verification-header">
+                    <h3>Pending Verfication</h3>
+                    <h3><Link to="#">See All</Link></h3>
+                </div>
+                <div className="verification-card-wrapper">
+                    <h4>Stuffed Animals</h4>
+                    <div className="verification-date-button">
+                        <p>May 30,2023 - From wishlist</p>
+                        <button>
+                            Verify
+                        </button>
+                    </div>
+                    <div className="icon-author">
+                        <i className="fa-solid fa-user"></i>
+                        <p>Greg Thomas</p>
+                    </div>
+                    
+                </div>
+                <div className="verification-card-wrapper">
+                    <h4>Stuffed Animals</h4>
+                    <div className="verification-date-button">
+                        <p>May 30,2023 - From wishlist</p>
+                        <button>
+                            Verify
+                        </button>
+                    </div>
+                    <div className="icon-author">
+                        <i className="fa-solid fa-user"></i>
+                        <p>Greg Thomas</p>
+                    </div>
+                    
+                </div>
+                <div className="verification-card-wrapper">
+                    <h4>Stuffed Animals</h4>
+                    <div className="verification-date-button">
+                        <p>May 30,2023 - From wishlist</p>
+                        <button>
+                            Verify
+                        </button>
+                    </div>
+                    <div className="icon-author">
+                        <i className="fa-solid fa-user"></i>
+                        <p>Greg Thomas</p>
+                    </div>
+                    
+                </div>
+                <div className="verification-card-wrapper">
+                    <h4>Stuffed Animals</h4>
+                    <div className="verification-date-button">
+                        <p>May 30,2023 - From wishlist</p>
+                        <button>
+                            Verify
+                        </button>
+                    </div>
+                    <div className="icon-author">
+                        <i className="fa-solid fa-user"></i>
+                        <p>Greg Thomas</p>
+                    </div>
+                    
+                </div>
+                
+           </div>
+        </div>
+        </div>
+    )
+}
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,11 @@
 
+import OrgDashBoard from '../../components/organization-dashboard';
 import './index.scss';
 
 export default function Home(){
     return(<>
             <h1>Home</h1>
+            <OrgDashBoard/>
     </>);
 };
 
