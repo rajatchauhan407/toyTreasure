af969282097658f3a83237cc33566941df9c29c6
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-06-19 04:03:06 -0700

    removed complonent for checking

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,11 +1,8 @@
-
-import OrgDashBoard from '../../components/organization-dashboard';
 import './index.scss';
 
 export default function Home(){
     return(<>
             <h1>Home</h1>
-            <OrgDashBoard/>
     </>);
 };
 
