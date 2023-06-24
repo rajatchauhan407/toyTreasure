578be981db858c7da64e59fe3dc132d44ff647a7
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-06-24 14:36:26 -0700

    completed wishlist table

---
+++ client/src/components/wishlist/index.js
@@ -0,0 +1,71 @@
+import "./index.scss";
+export default function OrgWishlist(){
+    let orgWishlistData = [
+        {
+            org_toy_category:"Stuffed Animal",
+            org_qty_required:38,
+            org_qty_received:45,
+            org_toy_pending:8
+        },
+        {
+            org_toy_category:"Stuffed Animal",
+            org_qty_required:38,
+            org_qty_received:45,
+            org_toy_pending:8
+        },
+        {
+            org_toy_category:"Stuffed Animal",
+            org_qty_required:38,
+            org_qty_received:45,
+            org_toy_pending:8
+        },
+        {
+            org_toy_category:"Stuffed Animal",
+            org_qty_required:38,
+            org_qty_received:45,
+            org_toy_pending:8
+        },
+        {
+            org_toy_category:"Stuffed Animal",
+            org_qty_required:38,
+            org_qty_received:45,
+            org_toy_pending:8
+        },
+        {
+            org_toy_category:"Stuffed Animal",
+            org_qty_required:38,
+            org_qty_received:45,
+            org_toy_pending:8
+        },
+        {
+            org_toy_category:"Stuffed Animal",
+            org_qty_required:38,
+            org_qty_received:45,
+            org_toy_pending:8
+        }
+    ]
+    return (<div className="table-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Tag Category</td>
+                            <td>Qty Required</td>
+                            <td>Qty Received</td>
+                            <td>Pending</td>
+                            <td>Icon</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orgWishlistData.map((el)=>{
+                            return <tr>
+                            <td>Stuffed Animals</td>
+                            <td>38</td>
+                            <td>26</td>
+                            <td>08</td>
+                            <td><i className="fa-solid fa-trash"></i></td>
+                        </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>);
+}
