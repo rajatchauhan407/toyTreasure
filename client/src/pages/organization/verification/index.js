fe73fe4f5636e1f4b16d441b1a86e5ed3617e070
cindycharvet <cindycharvetweb@gmail.com>
2023-06-27 13:27:12 -0700

    QR Code for verification UI

--- client/src/pages/organization/verification/index.js
+++ client/src/pages/organization/verification/index.js
@@ -1,6 +1,8 @@
+import OrgVerificationQRActive from "../../../components/org-verification-qr-active";
 import WrapperCardOrgTT13 from "../../../components/wrapper-vertification-pending-donor";
 export default function OrgVerification(){
     return (<>
             <WrapperCardOrgTT13/>
+            <OrgVerificationQRActive/>
     </>);
 }
