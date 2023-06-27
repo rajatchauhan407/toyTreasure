c71d524d054dab5dc7b7b256197fa9c6b03f8951
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-06-27 15:29:16 -0700

    created container for modal and user rewards modal

--- client/src/pages/organization/dashboard/index.js
+++ client/src/pages/organization/dashboard/index.js
@@ -1,6 +1,11 @@
-import OrgDashBoard from "../../../components/organization-dashboard"
+// import GeneralModalWrapper from "../../../components/general-modal-wrapper";
+import OrgDashBoard from "../../../components/organization-dashboard";
+// import UserRewardsModal from "../../../components/user-rewards-modal";
 export default function OrgDashboard(props){
     return(<>
         <OrgDashBoard/>
+        {/* <GeneralModalWrapper>
+                <UserRewardsModal/>
+        </GeneralModalWrapper> */}
     </>)
 }
