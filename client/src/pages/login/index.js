b743580e294a3b58403c4a03ed75b4d14e9fa457
Raha <r.emami98@gmail.com>
2023-06-26 22:55:42 -0700

    Login Page Compeleted

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,11 +1,18 @@
 import './index.scss';
 import LoginForm from '../../components/login';
+import LoginSidebar from '../../components/login-sidebar'
 export default function Login(){
     
 return (
     <>
+    <div className='loginWrapper'>
+    <LoginSidebar/>
+        <div className='loginFormWrapper'>
         <LoginForm/>
+        </div>
+    </div>
    </>
+   
     
 )
 }
