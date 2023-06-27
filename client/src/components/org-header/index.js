b018609971b81c4739334b22004b615a81dc4bcd
Raha <r.emami98@gmail.com>
2023-06-26 19:06:04 -0700

    Organization Header Added

---
+++ client/src/components/org-header/index.js
@@ -0,0 +1,15 @@
+import "./index.scss";
+
+
+function OrgHeader() {
+    return (
+      <header className="orgHeader">
+        <div className="orgHeaderProfile">
+            <h2 className="orgHeaderProfileName">Account User</h2>
+          <img className="orgHeaderProfilePhoto" src="https://picsum.photos/200" alt="Profile Photo of the Organization" />
+        </div>
+      </header>
+    );
+  }
+  
+  export default OrgHeader;
