1864f5205ba8f77ec3668fc11097c285409083dc
cindycharvet <cindycharvetweb@gmail.com>
2023-06-27 14:12:39 -0700

    Org Profile Refactor Fixing

--- client/src/components/org-profile-form/index.js
+++ client/src/components/org-profile-form/index.js
@@ -1,6 +1,5 @@
 // import MultiSelector from "../multi-selector";
 import "./index.scss"
-import OrgProfileDetails from "../org-profile-form-details";
 import OrgProfileImpact from "../org-profile-form-impact";
 import OrgProfileMedia from "../org-profile-form-media";
 import OrgProfileExtraD from "../org-profile-form-extradetails";
@@ -48,12 +47,74 @@ export default function OrgProfileForm() {
 return (
     <div className="OrgProfile">
         <h1>Organization Profile</h1>
-        <OrgProfileDetails/>
+            <form className="Details">
+                <label htmlFor="orgName">Organization Name</label><br />
+                <input 
+                    type="text" 
+                    id="orgName" 
+                    name="orgName"
+                    onChange={(e)=>{setOrgName(e.target.value)}}
+                    required /><br />
+    
+                <label htmlFor="orgDescription">Organization Description</label><br />
+                <textarea id="orgDescription" name="orgDescription" rows="4" cols="50" maxLength="250" required></textarea><br />
+    
+                <label htmlFor="orgCRA">CRA Registered Charity</label><br />
+                <input type="text" id="orgCRA" name="orgCRA" pattern="[a-zA-Z0-9]{1,15}" required /><br />
+    
+                <div className="one-line">
+                    <div className="sect-1">
+                        <label htmlFor="orgAddress1">Address Line 1</label><br />
+                        <input type="text" id="orgAddress1" name="orgAddress1" required /><br />
+
+                        <label htmlFor="orgCity">City</label><br />
+                        <input type="text" id="orgCity" name="orgCity" required /><br />
+
+                        <label htmlFor="orgProvince">Province</label><br />
+                        <select id="orgProvince" name="orgProvince" required>
+                            <option value="Alberta">Alberta</option>
+                            <option value="British Columbia">British Columbia</option>
+                            <option value="Manitoba">Manitoba</option>
+                            <option value="New Brunswick">New Brunswick</option>
+                            <option value="Newfoundland and Labrador">Newfoundland and Labrador</option>
+                            <option value="Northwest Territories">Northwest Territories</option>
+                            <option value="Nova Scotia">Nova Scotia</option>
+                            <option value="Nunavut">Nunavut</option>
+                            <option value="Ontario">Ontario</option>
+                            <option value="Prince Edward Island">Prince Edward Island</option>
+                            <option value="Quebec">Quebec</option>
+                            <option value="Saskatchewan">Saskatchewan</option>
+                            <option value="Yukon">Yukon</option>
+                        </select><br />
+                    </div>
+                    <div className="sect-2">
+                        <label htmlFor="orgAddress2">Address Line 2</label><br />
+                        <input type="text" id="orgAddress2" name="orgAddress2" required /><br />
+            
+                        <label htmlFor="orgZip">Zip Code</label><br />
+                        <input type="text" id="orgZip" name="orgZip" required /><br />
+            
+                        <label htmlFor="orgCountry">Country</label><br />
+                        <input type="text" id="orgCountry" name="orgCountry" value="Canada" disabled /><br />
+                    </div>
+                </div>
+        
+                <label htmlFor="orgEmail">Email</label><br />
+                <input type="email" id="orgEmail" name="orgEmail" required /><br />
+    
+                <label htmlFor="contactNumber">Contact Number</label><br />
+                <div className="phone-input">
+                    <input type="text" id="countryCode" name="countryCode" value="+1" disabled />
+                    <input type="number" id="phoneNumber" name="phoneNumber" required />
+                </div>
+            </form>
         <OrgProfileImpact/>
 
-        <OrgProfileMedia/>
-        <OrgProfileExtraD/>
-        <OrgProfileCategories/>
+        <div className="wrapper-right">
+            <OrgProfileMedia/>
+            <OrgProfileExtraD/>
+            <OrgProfileCategories/>
+        </div>
 
         <button type="submit" onClick={handleSubmit}>Save</button>
         <button type="reset">Reset</button>
