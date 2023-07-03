2b60f7f17022e2a03edfd61d6ee712c436ba2d20
Raha <r.emami98@gmail.com>
2023-07-03 16:40:37 -0700

    Components Done

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,23 @@
 import './index.scss';
+import HomeDashboardBadgeHistory from '../../components/donor-home-rewards-badge-history-Wrapper'
 
 export default function Home(){
     
     return(<>
          <h1>Home</h1>   
+         <div className='rewardsProfile'>
+            <div className='rewardsProfileHeader'>
+            <div>
+            <h4>Rewards Profile</h4>
+            </div>
+            <div>
+            <p>Your Points: 400</p>
+            </div>
+        </div>
+         
+         <HomeDashboardBadgeHistory/>
+         </div>
+         
     </>);
 };
 
