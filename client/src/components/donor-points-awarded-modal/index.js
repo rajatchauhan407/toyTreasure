b6a42f6d7df132ccdfddaaf5d22be81a56206447
cindycharvet <cindycharvetweb@gmail.com>
2023-07-04 00:53:14 -0700

    Component Done

---
+++ client/src/components/donor-points-awarded-modal/index.js
@@ -0,0 +1,17 @@
+import './index.scss';
+
+export default function DonorPointsModal(props){
+    return(
+        <div className="modalPoints">
+            <img src="https://picsum.photos/200/200?random=567" alt="" />
+            <h2>Congratulations!</h2>
+            <p>You have earned</p>
+            <div className='points-donor'>
+                <h3>200 points</h3>
+                <p>Thank you for your contribution to the community.</p>
+            </div>
+                <button className="go-rewards">Go to Rewards</button>
+                <a href="#">Home</a>
+        </div>
+    )
+}
