decbd790b7debc224f372a7fbccec9e9219a7bf0
MagdelineJannet <janet.magdeline@gmail.com>
2023-07-03 19:39:16 -0700

    store data in database

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,23 @@ import DonorDonationProcessList from './components/donor-donation-process-list';
 
 import Routing from "./routes/routing";
 function App() {
-  
+  //Array Object Desturcturing
+//   const details= {
+//     name:"Jannet",
+//     program:"WMDD",
+//     course:"HTML"
+//   }
+// const {name,program}=details;
+
+// const arr = ["Rajat",["Cindy","Raha","jannet"]]
+// const [myName,teamMembers]=arr;
+// console.log(myName);
+// console.log(teamMembers);
 
   return (
     <div className="App">
-        {/* <Routing/> */}
-        <DonorDonationProcessList/>
+        <Routing/>
+       
     </div>
     
   );
--- client/src/components/org-profile-form-details/index.js
+++ client/src/components/org-profile-form-details/index.js
@@ -1,21 +1,60 @@
 import "./index.scss";
+import {useState} from "react";
+import FireBaseFirestoreService from "../../services/Firebasefirestoreservice";
 
 export default function OrgProfileDetails(){
-
+const [orgName, setOrgName]= useState("ABC Toys");
+const [orgCRA, setOrgCRA]= useState("ORG CRA");
+const [orgDesc, setOrgDesc]= useState("Org Desc");
+function changeOrgName(event)
+{
+    setOrgName(event.target.value);
+}
+function changeOrgCRA(event)
+{
+    setOrgCRA(event.target.value);
+}
+// function changeOrgDesc(event)
+// {
+//     setOrgDesc(event.target.value);
+// }
+function saveData(e)
+{
+    e.preventDefault();
+    let formObject={
+        org_name:orgName,
+        orgCRA:orgCRA,
+        orgDesc:orgDesc
+    }
+    sendData(formObject);
+    console.log(formObject);
+}
+function sendData(obj)
+{
+  FireBaseFirestoreService.createDocument("organization_profile",obj);
+}
     return(
         <div className="OrgProfileDetails">
-            <form className="Details">
+            <form className="Details" onSubmit={saveData}>
                 <label htmlFor="orgName">Organization Name</label><br />
                 <input 
                     type="text" 
                     id="orgName" 
+                    value={orgName}
+                    onChange = {changeOrgName}
                     required /><br />
-    
+                <p>{orgName}</p>
                 <label htmlFor="orgDescription">Organization Description</label><br />
-                <textarea id="orgDescription" name="orgDescription" rows="4" cols="50" maxLength="250" required></textarea><br />
+                <textarea id="orgDescription" name="orgDescription" rows="4" cols="50" maxLength="250" 
+                value={orgDesc}
+                 onChange = {(e)=>{setOrgDesc(e.target.value); console.log(e);}}
+                required></textarea><br />
     
                 <label htmlFor="orgCRA">CRA Registered Charity</label><br />
-                <input type="text" id="orgCRA" name="orgCRA" pattern="[a-zA-Z0-9]{1,15}" required /><br />
+                <input type="text" id="orgCRA" 
+                 value={orgCRA}
+                 onChange = {changeOrgCRA} name="orgCRA" pattern="[a-zA-Z0-9]{1,15}" required /><br />
+                 <p>{orgCRA}</p>
     
                 <div className="one-line">
                     <div className="sect-1">
@@ -62,6 +101,7 @@ export default function OrgProfileDetails(){
                     <input type="text" id="countryCode" name="countryCode" value="+1" disabled />
                     <input type="number" id="phoneNumber" name="phoneNumber" required />
                 </div>
+                <button type="submit">Save</button>
             </form>
         </div>
 )
