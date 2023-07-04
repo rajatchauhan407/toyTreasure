c1f77e89da794ff90959c44815cfba55f9669a43
cindycharvet <cindycharvetweb@gmail.com>
2023-07-03 22:45:16 -0700

    Component Done

---
+++ client/src/components/donor-charity-profile-wishlist-banner/index.js
@@ -0,0 +1,11 @@
+import "./index.scss";
+
+
+export default function DonorCharityProfileWishlistBanner(props){
+    return(
+        <div className="WishlistBanner">
+            <h2>Help us fulfill our wishlists</h2>
+            <button>Donate to our wishlist now</button>
+        </div>
+    )
+}
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,11 @@
 import './index.scss';
+import DonorCharityProfileWishlistBanner from '../../components/donor-charity-profile-wishlist-banner';
 
 export default function Home(){
     
     return(<>
-         <h1>Home</h1>   
+         <h1>Home</h1> 
+         <DonorCharityProfileWishlistBanner/>  
     </>);
 };
 
