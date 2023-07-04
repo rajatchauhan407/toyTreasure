7737e3aa7297c100f2859efec8131b1f7bcca2a6
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 10:45:39 -0700

    removed unused vars

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,11 +1,10 @@
 import './index.scss';
-import DashboardPoints from '../../components/donor-home-dashboard-points';
+// import DashboardPoints from '../../components/donor-home-dashboard-points';
 
 export default function Home(){
     
     return(<>
         <h1>Home</h1>
-        <DashboardPoints/>
     </>);
 };
 
