69ec7f915cb992bffe6519587d270f1af5c3a4aa
Raha <r.emami98@gmail.com>
2023-07-03 17:37:02 -0700

    Donation Wish List Card Compeleted

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,13 @@
 import './index.scss';
+import DonationWishListCardWrapper from '../../components/donor-donation-wishList-card-wrapper';
 
 export default function Home(){
     
     return(<>
-         <h1>Home</h1>   
+         <h1>Home</h1>  
+         <h1>Wishlists</h1> 
+         <DonationWishListCardWrapper/>
+
     </>);
 };
 
