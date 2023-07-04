49ee99f272f62e31b90fdbbe5f87cdc6895cc2f1
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 11:15:55 -0700

    set home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,13 +1,8 @@
 import './index.scss';
-import DonationWishListCardWrapper from '../../components/donor-donation-wishList-card-wrapper';
-
 export default function Home(){
     
     return(<>
          <h1>Home</h1>  
-         <h1>Wishlists</h1> 
-         <DonationWishListCardWrapper/>
-
     </>);
 };
 
