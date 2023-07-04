b7e96a30c300226153a03643d7f0c939364b2925
cindycharvet <cindycharvetweb@gmail.com>
2023-07-03 23:33:15 -0700

    Component Fix

--- client/src/components/donor-donation-congrats-modal/index.js
+++ client/src/components/donor-donation-congrats-modal/index.js
@@ -6,9 +6,11 @@ export default function DonorDonationModal(props){
             <img src="https://picsum.photos/200/200?random=567" alt="" />
             <h2>Thank You</h2>
             <p>Your contribution is greatly appreciated and will bring joy and happiness to the children.</p>
-            <h3>Reward Instruction:</h3>
-            <p>After your donation is completed in site, scan the provided QR code given by the organization to earn the rewards points.</p>
-            <button className="go-home">Return to Home</button>
+            <div className='instructions'>
+                <h3>Reward Instruction:</h3>
+                <p>After your donation is completed in site, scan the provided QR code given by the organization to earn the rewards points.</p>
+            </div>
+                <button className="go-home">Return to Home</button>
         </div>
     )
 }
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,14 @@
 import './index.scss';
+import GeneralModalWrapper from '../../components/general-modal-wrapper';
+import DonorDonationModal from '../../components/donor-donation-congrats-modal';
 
 export default function Home(){
     
     return(<>
          <h1>Home</h1>   
+         <GeneralModalWrapper>
+            <DonorDonationModal/>
+         </GeneralModalWrapper>
     </>);
 };
 
