0d5afe0c3d8e8415b3ab9bfba516687a7fb49d95
cindycharvet <cindycharvetweb@gmail.com>
2023-07-03 22:30:27 -0700

    Component Done

--- client/src/components/donor-charity-profile-video/index.js
+++ client/src/components/donor-charity-profile-video/index.js
@@ -1,9 +1,19 @@
-import ".index.scss"
+import React from "react";
+import YouTube from "react-youtube";
+import "./index.scss";
 
-export default function DonorCharityProfileVideo(videoURL){
-    return(
-        <video src={videoURL}></video>
-    )
+export default function DonorCharityProfileVideo() {
+  const videoId = "4T7HwLGNiuw";
+
+  return (
+    <div className="CharityVideo">
+      <YouTube videoId={videoId} 
+      opts={{ 
+        width: "320", 
+        height: "240", 
+        playerVars: { autoplay: 1, muted: 1 } 
+        }} />
+    </div>
+  );
 }
-
    
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,11 @@
 import './index.scss';
+import DonorCharityProfileVideo from '../../components/donor-charity-profile-video';
 
 export default function Home(){
     
     return(<>
          <h1>Home</h1>   
+         <DonorCharityProfileVideo/>
     </>);
 };
 
