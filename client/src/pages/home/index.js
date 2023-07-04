d9f47487b03c2131fdd7b716f435e3fc94a226a4
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 13:47:50 -0700

    set homepage

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,16 +1,8 @@
 import './index.scss';
-import HomeDashboardRewardsCard from '../../components/donor-home-dashboard-rewards-card-wrapper';
-
 
 export default function Home(){
     return(<>
          <h1>Home</h1>
-
-         <div className='homeDashboardRewards'>
-            <h1>Rewards</h1>
-            
-          <HomeDashboardRewardsCard/>
-         </div>
     </>);
 };
 
