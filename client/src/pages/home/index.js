da4d944f90bfc5a3093b0a2b89581963776b2505
cindycharvet <cindycharvetweb@gmail.com>
2023-07-04 00:54:41 -0700

    Component Test

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,14 @@
 import './index.scss';
+import GeneralModalWrapper from '../../components/general-modal-wrapper';
+import DonorPointsModal from '../../components/donor-points-awarded-modal';
 
 export default function Home(){
     
     return(<>
-         <h1>Home</h1>   
+         <h1>Home</h1>
+         <GeneralModalWrapper>
+            <DonorPointsModal/> 
+         </GeneralModalWrapper>
     </>);
 };
 
