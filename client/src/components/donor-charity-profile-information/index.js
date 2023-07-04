681d9ed0bdc33aa8b79ed81e8ba6d9fe3c0f4e90
cindycharvet <cindycharvetweb@gmail.com>
2023-07-03 21:13:56 -0700

    First Commit

---
+++ client/src/components/donor-charity-profile-information/index.js
@@ -0,0 +1,18 @@
+import "./index.scss";
+
+export default function DonorCharityProfileInformation(props){
+    <div className="CharityInformation">
+        <div className="Information">
+            <img src="" alt="logo-charity" />
+            <h2>Organization Name</h2>
+            <p>Organization Description</p>
+            <button>Donate Now</button>
+        </div>
+        
+        <div className="Image">
+            <img src="" alt="main-image-charity" />
+        </div>
+
+    </div>
+
+}
