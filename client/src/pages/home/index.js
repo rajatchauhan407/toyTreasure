d1859ccab6552e0cb1c13b22f95da1ddb5d51650
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 11:03:24 -0700

    removed deendencies

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,11 +1,8 @@
 import './index.scss';
-import DonorCharityProfileWishlistBanner from '../../components/donor-charity-profile-wishlist-banner';
-
 export default function Home(){
     
     return(<>
          <h1>Home</h1> 
-         <DonorCharityProfileWishlistBanner/>  
     </>);
 };
 
