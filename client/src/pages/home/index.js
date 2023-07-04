4a96039814997f6313fe1fa2b5aff979a307239f
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 11:09:28 -0700

    removed unncessary imports

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,11 +1,8 @@
 import './index.scss';
-import DonorCharityProfileInformation from '../../components/donor-charity-profile-information';
-
 export default function Home(props){
     
     return(<>
          <h1>Home</h1>   
-         <DonorCharityProfileInformation/>
     </>);
 };
 
