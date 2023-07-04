2c51be39b4df20e28158906bfc8cd53258dd6bde
Raha <r.emami98@gmail.com>
2023-07-03 17:57:52 -0700

    Category Cards Done

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,13 @@
 import './index.scss';
+import DonationCategoryCardWrapper from '../../components/donor-donation-category-card-wrapper'
 
 export default function Home(){
     
     return(<>
          <h1>Home</h1>   
+         <h1>Categories</h1>
+         <p>Choose the category to donate if you can not find in wishlist.</p>
+         <DonationCategoryCardWrapper/>
     </>);
 };
 
