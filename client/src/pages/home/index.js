92211bce03e1e17615f18cdf3ea4a18404e627a3
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 11:07:28 -0700

    removed imports from home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,11 +1,8 @@
 import './index.scss';
-import DonorCharityProfileVideo from '../../components/donor-charity-profile-video';
-
 export default function Home(){
     
     return(<>
          <h1>Home</h1>   
-         <DonorCharityProfileVideo/>
     </>);
 };
 
