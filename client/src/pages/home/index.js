38b5015e012a367695d500bbe118c3d76ff4eaa6
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 10:52:28 -0700

    removing dependencies from home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,14 +1,9 @@
 import './index.scss';
-import GeneralModalWrapper from '../../components/general-modal-wrapper';
-import DonorPointsModal from '../../components/donor-points-awarded-modal';
 
 export default function Home(){
     
     return(<>
          <h1>Home</h1>
-         <GeneralModalWrapper>
-            <DonorPointsModal/> 
-         </GeneralModalWrapper>
     </>);
 };
 
