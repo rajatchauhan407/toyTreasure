cf163433f98e25ffe4574beec0da47a8d1bffa82
cindycharvet <cindycharvetweb@gmail.com>
2023-07-03 23:24:08 -0700

    Component Done

---
+++ client/src/components/donor-donation-congrats-modal/index.js
@@ -0,0 +1,14 @@
+import './index.scss';
+
+export default function DonorDonationModal(props){
+    return(
+        <div className="modalCongrats">
+            <img src="https://picsum.photos/200/200?random=567" alt="" />
+            <h2>Thank You</h2>
+            <p>Your contribution is greatly appreciated and will bring joy and happiness to the children.</p>
+            <h3>Reward Instruction:</h3>
+            <p>After your donation is completed in site, scan the provided QR code given by the organization to earn the rewards points.</p>
+            <button className="go-home">Return to Home</button>
+        </div>
+    )
+}
