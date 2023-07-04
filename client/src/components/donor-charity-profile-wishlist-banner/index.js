f6ada7a93d4cde4f0a23fa26d36fa59e91ac258e
cindycharvet <cindycharvetweb@gmail.com>
2023-07-03 22:56:39 -0700

    component css

--- client/src/components/donor-charity-profile-wishlist-banner/index.js
+++ client/src/components/donor-charity-profile-wishlist-banner/index.js
@@ -4,8 +4,10 @@ import "./index.scss";
 export default function DonorCharityProfileWishlistBanner(props){
     return(
         <div className="WishlistBanner">
-            <h2>Help us fulfill our wishlists</h2>
-            <button>Donate to our wishlist now</button>
+            <div className="WishlistBannerContent">
+                <h2>Help us fulfill our wishlists</h2>
+                <button>Donate to our wishlist now</button>
+            </div>
         </div>
     )
 }
