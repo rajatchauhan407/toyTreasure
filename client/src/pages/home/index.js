f33d04c666c3347e6efa8fe4450dc61e3368cf30
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 11:13:56 -0700

    set home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,13 +1,8 @@
 import './index.scss';
-import DonationCategoryCardWrapper from '../../components/donor-donation-category-card-wrapper'
-
 export default function Home(){
     
     return(<>
          <h1>Home</h1>   
-         <h1>Categories</h1>
-         <p>Choose the category to donate if you can not find in wishlist.</p>
-         <DonationCategoryCardWrapper/>
     </>);
 };
 
