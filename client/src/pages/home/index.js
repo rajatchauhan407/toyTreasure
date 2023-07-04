8b7d221c688027fbaa0226742bd205f22a53e344
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 10:56:49 -0700

    removed imports from home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,14 +1,9 @@
 import './index.scss';
-import GeneralModalWrapper from '../../components/general-modal-wrapper';
-import DonorDonationModal from '../../components/donor-donation-congrats-modal';
 
 export default function Home(){
     
     return(<>
          <h1>Home</h1>   
-         <GeneralModalWrapper>
-            <DonorDonationModal/>
-         </GeneralModalWrapper>
     </>);
 };
 
