7b52844bf1c1526230f6be5e31d2453c07684450
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-04 11:20:45 -0700

    set home page

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,23 +1,8 @@
 import './index.scss';
-import HomeDashboardBadgeHistory from '../../components/donor-home-rewards-badge-history-Wrapper'
-
 export default function Home(){
     
     return(<>
          <h1>Home</h1>   
-         <div className='rewardsProfile'>
-            <div className='rewardsProfileHeader'>
-            <div>
-            <h2>Rewards Profile</h2>
-            </div>
-            <div>
-            <p>Your Points: 400</p>
-            </div>
-        </div>
-         
-         <HomeDashboardBadgeHistory/>
-         </div>
-         
     </>);
 };
 
