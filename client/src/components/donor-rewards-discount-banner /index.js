679714239f3019ff8a06e71156ffd107dde5fbaf
cindycharvet <cindycharvetweb@gmail.com>
2023-07-04 17:59:25 -0700

    Progress component

--- client/src/components/donor-rewards-discount-banner /index.js
+++ client/src/components/donor-rewards-discount-banner /index.js
@@ -3,10 +3,14 @@ import './index.scss';
 export default function discountCard(props){
     return(
         <div className="discountCard">
-            <img src="https://picsum.photos/200/200?random=567" alt="" />
-            <h2>Get the best cuddle in the whole wide world for your kid</h2>
-            <h3 className="Discount">20% off </h3>
-            <p>on your first order</p>
+            <div className="imageBanner">
+                <img src="https://picsum.photos/200/200?random=567" alt="" />
+            </div>
+            <div className="infoBanner">
+                <h2>Get the best cuddle in the whole wide world for your kid</h2>
+                <h3 className="Discount">20% off </h3>
+                <p>on your first order</p>
+            </div>
         </div>
     )
 }
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,7 +1,12 @@
 import './index.scss';
+import discountCard from '../../components/donor-rewards-discount-banner ';
+
 export default function Home(){
     return(<>
          <h1>Home</h1>
+          <discountCard/>
     </>);
 };
+
+
 
