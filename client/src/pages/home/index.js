1c10f404827b69875016a268f5a47d661d7f22cf
cindycharvet <cindycharvetweb@gmail.com>
2023-07-04 17:20:23 -0700

    Remove component from the home

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,15 +1,9 @@
 import './index.scss';
-import GeneralModalWrapper from '../../components/general-modal-wrapper';
-import UserRewardsModal from '../../components/user-rewards-modal';
 
 export default function Home(){
     
     return(<>
-         <h1>Home</h1> 
-         <GeneralModalWrapper>
-          <UserRewardsModal/>
-        </GeneralModalWrapper>  
-         
+         <h1>Home</h1>  
     </>);
 };
 
