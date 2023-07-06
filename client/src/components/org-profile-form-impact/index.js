2977df812740a470405b2318c7fd6e651acc2423
Raha <r.emami98@gmail.com>
2023-07-06 15:45:14 -0700

    Year Founded added

--- client/src/components/org-profile-form-impact/index.js
+++ client/src/components/org-profile-form-impact/index.js
@@ -1,16 +1,34 @@
 import "./index.scss";
+import { useState } from "react";
+import FireBaseFirestoreService from '../../services/Firebasefirestoreservice'
 
 export default function OrgProfileImpact(){
+    const [yearFounded, setYearFounded] = useState('');
+    const submitImpactButton = (e)=>{
+        e.preventDefault();
+        const impactDetails = {
+            founded_year: yearFounded
+        }
+        FireBaseFirestoreService.createDocument("organization_mission", impactDetails)
+
+}
     return(
     <div className="OrgProfileImpact">
-                <form className= "Impact">
+                <form className= "Impact" onSubmit={submitImpactButton}>
                     <h2>Impact of Organization</h2>
                     <p>Add the numbers of impact in your organization</p>
 
                     <div className="statistics">
                         <div className="Section1">
                             <label htmlFor="yearFounded">Year Founded</label> <br />
-                            <input type="number" id="yearFounded" name="yearFounded" min="1900" max="2023" required /> <br />
+                            <input 
+                            type="number" 
+                            onChange={(e)=>{setYearFounded(e.target.value)}} 
+                            id="yearFounded" 
+                            name="yearFounded" 
+                            min="1900" 
+                            max="2023" 
+                            required /> <br />
 
                             <label htmlFor="numDonors">Donors</label> <br />
                             <input type="number" id="numDonors" name="numDonors" min="0" required /> <br />
@@ -27,6 +45,7 @@ export default function OrgProfileImpact(){
                             <input type="number" id="numKidsSupported" name="numKidsSupported" min="0" required /> <br />
                         </div>
                     </div>
+                    <button type="submit">Submit</button>
                 </form>
     </div>
 ) 
