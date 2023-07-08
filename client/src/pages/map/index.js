33b02f66a67139cc7328c7254d60ccf1a71665d2
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-08 02:48:23 -0700

    map done only a bit of working remaining

--- client/src/pages/map/index.js
+++ client/src/pages/map/index.js
@@ -1,20 +1,11 @@
 import './index.scss';
 import InteractiveMap from '../../components/imap';
 import OrgMapCard from '../../components/org-map-card';
-import { useRef } from 'react';
+import { useRef,useState } from 'react';
 import OrgMapDescCard from '../../components/org-map-desc-card';
 export default function Map(){
-    const orgMapRefs = useRef([]);
-
-    function handleRef(index){
-        console.log(index);
-        console.log(orgMapRefs.current);
-        if(orgMapRefs.current[index].style.backgroundColor==="hotpink"){
-            orgMapRefs.current[index].style.backgroundColor="unset";
-        }else{
-            orgMapRefs.current[index].style.backgroundColor="hotpink";
-        }
-    }
+    const [selectedIndex, setSelectedIndex] = useState(null);
+    const [isOrgDescShown, setIsOrgDescShown] = useState(false);
     let orgs = [
         {orgName:"Sharing Smiles",
          address:"123 very long street name North Vancouver BC V5V 8H8",
@@ -47,6 +38,7 @@ export default function Map(){
          time:"9:00 - 18:00"
         }
     ];
+
     const SideHeader = ()=>{
         return <div className="tt-62-side-wrapper">
                 <h1>Search</h1>
@@ -77,14 +69,18 @@ export default function Map(){
                     <div className='tt-62-org-card-wrapper'
                     >
                     {orgs.map((el,index)=>{
-                        return <div onClick={()=>{handleRef(index)}}>
+                        return <div 
+                        onClick={()=>{setSelectedIndex(index); setIsOrgDescShown(true)}}
+                        className={selectedIndex === index?'background':''}
+                        >
                         <OrgMapCard
                         orgName={el.orgName}
                         address={el.address}
                         days={el.days}
                         time={el.time}
                         key={index}
-                        ref={(el)=>orgMapRefs.current.push(el)}  
+                        
+                        
                     />
                     </div>
                     })
@@ -93,7 +89,8 @@ export default function Map(){
                 </div>
                 <div className='tt-62-right-map'>
                     <InteractiveMap/>
-                    <OrgMapDescCard/>
+                    {isOrgDescShown?<OrgMapDescCard/>:""}
+                    
                 </div>   
         </div>
 
