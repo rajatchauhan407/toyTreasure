6d47b272bd47e87d471c6d10e472a25c975b8211
MagdelineJannet <janet.magdeline@gmail.com>
2023-07-09 20:03:04 -0700

    donor rewards fetched from firestore

--- client/src/components/donor-home-dashboard-rewards-card/index.js
+++ client/src/components/donor-home-dashboard-rewards-card/index.js
@@ -1,13 +1,18 @@
 import "./index.scss";
-export default function HomeDashboardRewards(props){
+export default function HomeDashboardRewards(props){   
+        
+    let state = props.status.toString();
+       
     return (
-            <div className="homeDashboardRewardsCardWrapper">
+            <div className="homeDashboardRewardsCardWrapper" id={state}>
+              
                 <p className="homeDashboardRewardsPoints">
                        {props.points}
                     </p>
                     <div className="homeDashboardRewardsCardImgWrapper">
-                        <img src={props.imageUrl} alt=""/>
+                        <img src={props.imageUrl} alt=""/>                       
                     </div>
+                    {props.status}
                     <button>Redeem now</button>
             </div>
     )
