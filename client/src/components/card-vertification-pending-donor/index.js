1aca336393b15a2a9c2a4e9bcdc505c437e66876
cindycharvet <cindycharvetweb@gmail.com>
2023-07-11 04:04:07 -0700

    Fetch of data to the component

--- client/src/components/card-vertification-pending-donor/index.js
+++ client/src/components/card-vertification-pending-donor/index.js
@@ -1,18 +1,81 @@
 import "./index.scss";
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import FireBaseFirestoreService from "../../services/Firebasefirestoreservice";
 
-export default function CardOrgTT13(props){
-    const navigate = useNavigate();
-        return(
-                <div className="listInfoCardOrgTT13">
-                    <ul> 
-                        <li>{props.orderNumber}</li>
-                        <li>{props.name}</li>
-                        <li>{props.method}</li>
-                        <li>{props.date}</li>
-                        <li>{props.Qty}</li>
-                        <li>{props.donationStatus}</li>
-                        <li><button onClick={()=>{navigate('/organization/verification/3')}}>Verify</button></li>
-                    </ul>
-                </div>
-        )}
+export default function CardOrgTT13() {
+  const [orgVerificationList, setOrgVerificationList] = useState([]);
+
+  async function getOrgVerificationListData() {
+    try {
+      const data = await FireBaseFirestoreService.getDocumentsInArray("user_donations");
+      setOrgVerificationList(data || []);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    getOrgVerificationListData();
+  }, []);
+
+  const navigate = useNavigate();
+
+  const statusMapping = {
+    cancelled: "Cancelled",
+    completed: "Completed",
+    pending: "Pending",
+  };
+
+  const deliveryMethodMapping = {
+    drop_off: "Drop-off",
+    pickup: "Pick-Up",
+  };
+
+  const getStatusText = (statusObj) => {
+    if (!statusObj) {
+      return "N/A";
+    }
+
+    const trueStatus = Object.keys(statusObj).find((key) => statusObj[key]);
+    return statusMapping[trueStatus] || "N/A";
+  };
+
+  const getDeliveryMethodText = (methodObj) => {
+    if (!methodObj) {
+      return "N/A";
+    }
+
+    const trueMethod = Object.keys(methodObj).find((key) => methodObj[key]);
+    return deliveryMethodMapping[trueMethod] || "N/A";
+  };
+
+  const formatDate = (timestamp) => {
+    if (!timestamp) {
+      return "N/A";
+    }
+    const dateObj = timestamp.toDate();
+    const options = { month: 'long', day: 'numeric', year: 'numeric' };
+    return dateObj.toLocaleDateString(undefined, options);
+  };
+
+  return (
+    <tbody>
+      {orgVerificationList.map((data) => (
+        <tr key={data.id}>
+          <td>{data.orderNumber}</td>
+          <td>{data.name}</td>
+          <td>{getDeliveryMethodText(data.method)}</td>
+          <td>{formatDate(data.date)}</td>
+          <td>{data.Qty}</td>
+          <td>{getStatusText(data.donationStatus)}</td>
+          <td>
+            <button onClick={() => { navigate('/organization/verification/3') }}>
+              Verify
+            </button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
--- client/src/components/wrapper-vertification-pending-donor/index.js
+++ client/src/components/wrapper-vertification-pending-donor/index.js
@@ -1,50 +1,43 @@
-import './index.scss'
-import CardOrgTT13 from '../card-vertification-pending-donor'
+import './index.scss';
+import CardOrgTT13 from '../card-vertification-pending-donor';
 
-function WrapperCardOrgTT13() {
-  const verification = [
-    {orderNumber: 37890, name:"Greg Thomas", method: "drop-off", date: "05/05/2023", Qty: 6, donationStatus: "Pending"},
-    {orderNumber: 37890, name:"Greg Thomas", method: "drop-off", date: "05/05/2023", Qty: 6, donationStatus: "Pending"},
-    {orderNumber: 37890, name:"Greg Thomas", method: "drop-off", date: "05/05/2023", Qty: 6, donationStatus: "Pending"},
+const verificationData = [
+  {
+    orderNumber: 37890,
+    name: "Greg Thomas",
+    method: "drop-off",
+    date: "05/05/2023",
+    Qty: 6,
+    donationStatus: "Pending"
+  },
 ];
 
-    return <div className="Verification">
+export default function WrapperCardOrgTT13() {
+  return (
+    <div className="Verification">
       <h1>Verification</h1>
       <div className="org-verification-donation-req">
-       <h2>Donation Requests</h2>
-       <div id="org-verification-sort-container">
-          <button id="org-vertification-sort-button">Sort By</button>
-          {/* <ul id="org-verification-sort-options">
-            <li data-sort="date">Date</li>
-            <li data-sort="donationStatus">Donation Status</li>
-          </ul> */}
-        </div>
+        <h2>Donation Requests</h2>
       </div>
-      
 
-      <div className="TitleCardOrgTT13">
-              <ul>
-                  <li>Order Number</li>
-                  <li>Name</li>
-                  <li>Method</li>
-                  <li>Date</li>
-                  <li>Qty</li>
-                  <li>Donation Status</li>
-                  <li>Action</li>
-              </ul>
-      </div>
-      
-      {verification.map((el)=>{
-          
-                return <CardOrgTT13 
-                          orderNumber={el.orderNumber}
-                          name={el.name}
-                          method={el.method}
-                          date={el.date}
-                          Qty={el.Qty}
-                          donationStatus={el.donationStatus}
-                          />
-            })}
-            </div>
-          }
-export default WrapperCardOrgTT13;
+      <table className="TableCardOrgTT13">
+        <thead>
+          <tr>
+            <th>Order Number</th>
+            <th>Name</th>
+            <th>Method</th>
+            <th>Date</th>
+            <th>Qty</th>
+            <th>Donation Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {verificationData.map((data) => (
+            <CardOrgTT13 key={data.orderNumber} data={data} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
