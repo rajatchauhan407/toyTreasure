9c98319f0a431d0403caacf6073616c5ffcca794
cindycharvet <cindycharvetweb@gmail.com>
2023-07-11 01:59:19 -0700

    Reverse changes

--- client/src/components/org-verification-card/index.js
+++ client/src/components/org-verification-card/index.js
@@ -1,44 +1,31 @@
 import "./index.scss";
-import { useState,useEffect } from 'react';
-import FireBaseFirestoreService from "../../services/Firebasefirestoreservice";
 
 export default function OrgVerificationCard(){
-    const [orgVerificationList, setOrgVerificationList] = useState([]); 
-        async function getOrgVerificationListData()
+
+    let orgVerificationData = [
         {
-            const data = await FireBaseFirestoreService.getDocumentsInArray("user_donations");
-            let array = data;       
-            setOrgVerificationList(array);       
+            animalType:"Stuffed Animal",
+            date:"23-05-89",
+            name:"Greg Thomas"
+        },
+        {
+            animalType:"Stuffed Animal",
+            date:"23-05-89",
+            name:"Greg Thomas"
+        },
+        {
+            animalType:"Stuffed Animal",
+            date:"23-05-89",
+            name:"Greg Thomas"
+        },
+        {
+            animalType:"Stuffed Animal",
+            date:"23-05-89",
+            name:"Greg Thomas"
         }
-        useEffect(()=>{
-            getOrgVerificationListData();
-        },[]);
-
-
-    // let orgVerificationData = [
-    //     {
-    //         animalType:"Stuffed Animal",
-    //         date:"23-05-89",
-    //         name:"Greg Thomas"
-    //     },
-    //     {
-    //         animalType:"Stuffed Animal",
-    //         date:"23-05-89",
-    //         name:"Greg Thomas"
-    //     },
-    //     {
-    //         animalType:"Stuffed Animal",
-    //         date:"23-05-89",
-    //         name:"Greg Thomas"
-    //     },
-    //     {
-    //         animalType:"Stuffed Animal",
-    //         date:"23-05-89",
-    //         name:"Greg Thomas"
-    //     }
-    // ];
+    ];
     return (
-            orgVerificationList.map((el)=>{
+            orgVerificationData.map((el)=>{
                 return <div className="verification-card-wrapper">
                     <h4>{el.animalType}</h4>
                     <div className="verification-date-button">
