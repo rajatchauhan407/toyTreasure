4bcecc715ca977d8b5a623c3ed8a2144a02d0f0e
Raha <r.emami98@gmail.com>
2023-07-10 23:56:32 -0700

    v1 partially done

--- client/src/components/donor-create-account/index.js
+++ client/src/components/donor-create-account/index.js
@@ -1,22 +1,105 @@
 import './index.scss';
+import { useState, useContext } from "react";
+import FireBaseAuthService from '../../services/FirebaseAuthService';
+import AuthContext from '../../services/auth-context';
+import FireBaseFirestoreService from '../../services/Firebasefirestoreservice';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 export default function CreateAccountDonor(){
+    const authContext = useContext(AuthContext);
+    const [fullName, setFullName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [agreeTerms, setAgreeTerms] = useState(false);
+
+    const handleCreateAccount = async (signup)=>{
+        signup.preventDefault();
+        const { uid } = FireBaseAuthService.auth.currentUser;
+        const userInfo = {
+            displayName: fullName,
+            phoneNumber: phoneNumber,
+            email: email,
+            uid: uid,
+            user_Type: "Donor",
+        }
+        FireBaseFirestoreService.createDocument("user", userInfo);
+        if(!fullName || !phoneNumber || !email || !password || !confirmPassword || !agreeTerms){
+            console.log('Please fill in all fields.');
+            return;
+        }
+
+        if(password !== confirmPassword){
+            console.log( 'Passwords do not match.');
+            return;
+        }
+
+        try{
+            // await FireBaseAuthService.createUserWithEmailAndPassword(auth,email, password);
+            console.log('account created sucessfully.');
+        } catch (error){
+            console.error('Error creating account', error)
+        }
+    };
     return<>
     <div className='tt-84-container'>
     <h1>Join as Donor</h1>
     <p>sign up here to join us for great change</p>
-    <form action="">
-    <label htmlFor="">Your Fullname</label>
-        <input type="text" placeholder='Enter Your Name'/>
-        <label htmlFor="">Phone Number</label>
-        <input type="number" placeholder='Enter Your Phone Number'/>
-        <label htmlFor="">Email</label>
-        <input type="text" placeholder='Enter Your Email Adress'/>
-        <label htmlFor="">Create Password</label>
-        <input type="text" placeholder='Password'/>
-        <label htmlFor="">Confirm Password</label>
-        <input type="text" placeholder='Password'/>
-        <input type="checkbox" value='I agree to terms and conditions'/>
+    <form onSubmit={handleCreateAccount}>
+    <label htmlFor="fullName">Your Fullname</label>
+        <input
+        type="text" 
+        placeholder='Enter Your Name'
+        id='fullName'
+        value={fullName}
+        onChange={(signup)=> setFullName(signup.target.value)}
+        />
+
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <input 
+        type="number" 
+        placeholder='Enter Your Phone Number'
+        id='phoneNumber'
+        value={phoneNumber}
+        onChange={(signup)=> setPhoneNumber(signup.target.value)}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input 
+        type="text" 
+        placeholder='Enter Your Email Adress'
+        id='email'
+        value={email}
+        onChange={(signup)=> setEmail(signup.target.value)}
+        />
+
+        <label htmlFor="password">Create Password</label>
+        <input 
+        type="password" 
+        placeholder='Password'
+        id='password'
+        value={password}
+        onChange={(signup)=>setPassword(signup.target.value)}
+        />
+
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input 
+        type="password" 
+        placeholder='Password'
+        id='confirmPassword'
+        value={confirmPassword}
+        onChange={(signup)=>setConfirmPassword(signup.target.value)}
+        />
+
+        <input 
+        type="checkbox" 
+        id='agreeTerms'
+        checked={agreeTerms}
+        onChange={(signup)=> setAgreeTerms(signup.target.checked)}
+        />
+        <label htmlFor='agreeTerms'>Agree to Terms and conditions.</label>
+
         <button type="submit">Create account</button>
         <p>Already have an account?</p><Link to="/login">Sign In</Link>
     </form>
