a073d829e3e7723124370c9b01d13d448d4f381a
cindycharvet <cindycharvetweb@gmail.com>
2023-07-11 15:31:54 -0700

    Modal almost done

---
+++ client/src/components/org-wishlist-confirmation-modal/index.js
@@ -0,0 +1,13 @@
+import './index.scss'
+
+export default function OrgWishlistConfirmModal(props){
+    return(
+        <div className="modalConfirmWishlist">
+            <img src="https://picsum.photos/200/200?random=567" alt="" />
+            <h2>Congrats! Toy added to Wishlist.</h2>
+            <p>Your new toy wishlist is successfully added.</p>
+            <button className="go-dashboard">Go to Dashboard</button>
+            <button className="go-wishlist">Add a new toy</button>
+        </div>
+    )
+}
