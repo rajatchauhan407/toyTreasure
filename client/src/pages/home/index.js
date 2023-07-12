54c215f8985a15160b0627e37a46201505fecf24
cindycharvet <cindycharvetweb@gmail.com>
2023-07-11 17:50:19 -0700

    Functionality Done

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,15 +1,37 @@
 import './index.scss';
+import AuthContext from '../../services/auth-context';
+import React, {useContext} from "react";
+
+import { useEffect, useState } from 'react';
+import FireBaseFirestoreService from "../../services/Firebasefirestoreservice";
+
 import DashboardPoints from '../../components/donor-home-dashboard-points';
 import DonorHomeDashboardMap from '../../components/donor-home-dashboard-map';
 import HomeDashBoardStories from '../../components/home-dashboard-stories';
 import HomeDashboardRewardsCard from '../../components/donor-home-dashboard-rewards-card-wrapper';
 import DiscountCard from '../../components/donor-rewards-discount-banner';
-import AuthContext from '../../services/auth-context';
-import React, {useContext} from "react";
+import DashboardPendingDonation from '../../components/donor-home-dashboard-pending-donation';
+
+
 
 export default function Home(){
   const userNameAuth = useContext(AuthContext);
-  
+  const [hasPendingDonation, setHasPendingDonation] = useState(false);
+
+  useEffect(() => {
+    async function checkPendingDonation() {
+      try {
+        const data = await FireBaseFirestoreService.getDocumentsInArray("user_donations");
+        const hasPending = data.some((donation) => donation.user_donation_status.pending === true);
+        setHasPendingDonation(hasPending);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    checkPendingDonation();
+  }, []);
+
     let discountData = [
         {
           url: 'https://picsum.photos/400/400?rand=431',
@@ -33,6 +55,12 @@ export default function Home(){
             <h1>Hi {userNameAuth.displayName}, make your first donation</h1>
             <DashboardPoints/>
         </div>
+
+        {hasPendingDonation && (
+          <div className='pending-donation'>
+            <DashboardPendingDonation/>
+          </div>
+        )}
 
         <div className='home-map'>
             <DonorHomeDashboardMap/>
