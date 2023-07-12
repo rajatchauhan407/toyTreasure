00981eb09dfc68c48db0cef2bc4887451b38a442
cindycharvet <cindycharvetweb@gmail.com>
2023-07-11 21:10:50 -0700

    working but you can merge

--- client/src/components/donor-charity-profile-information/index.js
+++ client/src/components/donor-charity-profile-information/index.js
@@ -1,19 +1,40 @@
 import "./index.scss";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import FireBaseFirestoreService from "../../services/Firebasefirestoreservice";
+
 export default function DonorCharityProfileInformation(){
-    return(
+    const [orgCharityProfile, setOrgCharityProfile] = useState([]);
+
+  useEffect(() => {
+    async function getOrgCharityProfileData() {
+      try {
+        const data = await FireBaseFirestoreService.getDocumentsInArray("organization_profile");
+        setOrgCharityProfile(data || []);
+        console.log(data)
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    getOrgCharityProfileData();
+  }, []);
+
+
+  return (
     <div className="CharityInformation">
-        <div className="Information">
-            <img src="" alt="logo-charity" />
-            <h2>Organization Name</h2>
-            <p>Organization Description</p>
-            <button><Link to="/donation/toys">Donate Now</Link></button>
-        </div>
-        
+        {orgCharityProfile.map(profile => (
+            <div key={profile.id} className="Information">
+                <img src="" alt="logo-charity" />
+                <h2>{profile.org_name}</h2>
+                <p>Organization Description</p>
+                <button><Link to="/donation/toys">Donate Now</Link></button>
+            </div>
+        ))}
+
         <div className="Image">
             <img src="" alt="main-charity" />
         </div>
-
     </div>
-    )
+    );
 }
