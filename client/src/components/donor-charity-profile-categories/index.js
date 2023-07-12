2f532ab98c9e612b56740b49078e8dd095212c72
cindycharvet <cindycharvetweb@gmail.com>
2023-07-12 02:08:07 -0700

    error in categories trying to fix

--- client/src/components/donor-charity-profile-categories/index.js
+++ client/src/components/donor-charity-profile-categories/index.js
@@ -1,14 +1,35 @@
-import "./index.scss";
+import './index.scss';
+import { useState, useEffect } from 'react';
+import FireBaseFirestoreService from '../../services/Firebasefirestoreservice';
 
-export default function CharityProfileCategoryCard(props) {
+function CharityProfileCategoryCard() {
+  const [orgCategory, setOrgCategory] = useState([]);
+
+  async function getOrgCategoryData() {
+    const orgCategoryData = await FireBaseFirestoreService.getDocumentsInArray("toys_categories");
+    let array = orgCategoryData.map((item) => ({
+      ...item,
+      quantity: 0
+    }));
+    setOrgCategory(array);
+  }
+
+  useEffect(() => {
+    getOrgCategoryData();
+  }, []);
+
   return (
     <div className="CharityProfileCategoryCardWrapper">
-      <div className="CharityProfileCategoryCardImgWrapper">
-        <img src={props.imageUrl} alt="" />
-        <p className="CharityProfileCategoryCardPoints">{props.points}</p>
-      </div>
-      <h4>{props.toyName}</h4>
-      <p>{props.toyType}</p>
+      {orgCategory.map((el, index) => (
+        <div key={index} className="CharityProfileCategoryCardImgWrapper">
+          <div className="CharityProfileCategoryCardImgWrapper">
+            <img src={el.category_picture} alt="" />
+            <p className="CharityProfileCategoryCardPoints">{el.category_points}</p>
+          </div>
+          <h4>{el.category_name}</h4>
+          <p>{el.category_age}</p>
+        </div>
+      ))}
     </div>
   );
 }
