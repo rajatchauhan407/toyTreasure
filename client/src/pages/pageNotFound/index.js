ca6d347935887dc69ee1a55ee3c57118f400e552
cindycharvet <cindycharvetweb@gmail.com>
2023-07-11 20:02:49 -0700

    page styling

--- client/src/pages/pageNotFound/index.js
+++ client/src/pages/pageNotFound/index.js
@@ -1,10 +1,10 @@
 import './index.scss';
 export default function PageNotFound(){
     return(<>
-        <div className='404-page'>
+        <div className='errorPage'>
             <h1>error 404</h1>
             <h3>Page is not found</h3>
-            <p>the page doesn’t exist or was deleted</p>
+            <p>the page doesn't exist or was deleted</p>
             <button>Back to Home</button>
         </div>
         </>);
