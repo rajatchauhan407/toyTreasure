5af94a34597b6606ac0376c8992b095c994c083c
cindycharvet <cindycharvetweb@gmail.com>
2023-07-11 18:07:13 -0700

    Functionality Done plus opening the camera

--- client/src/components/donor-home-dashboard-pending-donation/index.js
+++ client/src/components/donor-home-dashboard-pending-donation/index.js
@@ -1,49 +1,81 @@
 import './index.scss';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import FireBaseFirestoreService from "../../services/Firebasefirestoreservice";
 
-// const tt72pendingDonation = [
-//     {pending:"one",dateandtime:"June 18th, 2023 (2:00 pm - 4:00 pm)"}
-// ]
-
 export default function DashboardPendingDonation() {
-    const [donorPendingDonation, setPendingDonation] = useState([]);
-  
-    useEffect(() => {
-      async function getPendingDonationData() {
-        try {
-          const data = await FireBaseFirestoreService.getDocumentsInArray("user_donations");
-          const pendingDonations = data.filter((donation) => donation.user_donation_status.pending === true);
-          setPendingDonation(pendingDonations || []);
-          console.log(pendingDonations);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
+  const [donorPendingDonation, setPendingDonation] = useState([]);
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    async function getPendingDonationData() {
+      try {
+        const data = await FireBaseFirestoreService.getDocumentsInArray("user_donations");
+        const pendingDonations = data.filter((donation) => donation.user_donation_status.pending === true);
+        setPendingDonation(pendingDonations || []);
+        console.log(pendingDonations);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-  
-      getPendingDonationData();
-    }, []);
-  
-    const formatDate = (timestamp) => {
-      const dateObj = timestamp.toDate();
-      const options = { month: 'long', day: 'numeric', year: 'numeric' };
-      return dateObj.toLocaleDateString(undefined, options);
-    };
-  
-    return (
-      <div className='tt-72-DashboardPendingDonationWrapper'>
-        <div className="tt-72-DashboardPendingDonation">
-          <h3>You have a Pending Donation</h3>
-          {donorPendingDonation.map((donation, index) => (
-            <div key={index}>
-              <h3>{donation.org_name}</h3>
-              <p>{formatDate(donation.user_donation_date)}</p> {/* Convert Timestamp to a renderable format */}
-            </div>
-          ))}
-          <input type="button" value="Scan QR Code" id="tt72scanQR" className="tt72scanQR" />
-        </div>
+    }
+
+    getPendingDonationData();
+  }, []);
+
+  const formatDate = (timestamp) => {
+    const dateObj = timestamp.toDate();
+    const options = { month: 'long', day: 'numeric', year: 'numeric' };
+    return dateObj.toLocaleDateString(undefined, options);
+  };
+
+  const handleScanQRCode = () => {
+    setIsCameraOpen(true);
+
+    // Access the camera using getUserMedia
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      })
+      .catch((error) => {
+        console.error("Error accessing camera:", error);
+        setIsCameraOpen(false);
+      });
+  };
+
+  const handleStopCamera = () => {
+    const stream = videoRef.current.srcObject;
+    const tracks = stream && stream.getTracks();
+    if (tracks) {
+      tracks.forEach((track) => track.stop());
+    }
+    setIsCameraOpen(false);
+  };
+
+  return (
+    <div className='tt-72-DashboardPendingDonationWrapper'>
+      <div className="tt-72-DashboardPendingDonation">
+        <h3>You have a Pending Donation</h3>
+        {donorPendingDonation.map((donation, index) => (
+          <div key={index}>
+            <h3>{donation.org_name}</h3>
+            <p>{formatDate(donation.user_donation_date)}</p>
+          </div>
+        ))}
+        <input
+          type="button"
+          value="Scan QR Code"
+          id="tt72scanQR"
+          className="tt72scanQR"
+          onClick={handleScanQRCode}
+        />
+        {isCameraOpen && (
+          <div>
+            <video ref={videoRef} autoPlay />
+            <button onClick={handleStopCamera}>Stop Camera</button>
+          </div>
+        )}
       </div>
-    );
-  }
-  
-  
+    </div>
+  );
+}
