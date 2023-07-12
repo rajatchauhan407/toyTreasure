d478f23965006b8ddbf76b0b8cb7dec5d5918722
cindycharvet <cindycharvetweb@gmail.com>
2023-07-11 19:53:00 -0700

    page404

--- client/src/pages/pageNotFound/index.js
+++ client/src/pages/pageNotFound/index.js
@@ -1,6 +1,11 @@
 import './index.scss';
 export default function PageNotFound(){
     return(<>
-            <h1>PageNotFound</h1>
+        <div className='404-page'>
+            <h1>error 404</h1>
+            <h3>Page is not found</h3>
+            <p>the page doesn’t exist or was deleted</p>
+            <button>Back to Home</button>
+        </div>
         </>);
 }
