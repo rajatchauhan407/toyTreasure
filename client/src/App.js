7ec29192039c59720fe7c51477573ca0d12a223d
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-11 18:12:35 -0700

    removed code from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
-import { useContext } from 'react';
 import './App.css';
 
 import Routing from "./routes/routing";
-import AuthContext from './services/auth-context';
 function App() {
-let authCtx = useContext(AuthContext);
-console.log(authCtx);
   return (
     <div className="App">
         <Routing/>
