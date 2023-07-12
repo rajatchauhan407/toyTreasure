d07e5a83d72ee72b2586e449c54d554311985e18
cindycharvet <cindycharvetweb@gmail.com>
2023-07-12 00:50:00 -0700

    impact cards fetching data done

--- client/src/components/org-data-card/index.js
+++ client/src/components/org-data-card/index.js
@@ -1,39 +1,85 @@
 import "./index.scss";
-export default function OrgDataCard(){
+import { useEffect, useState } from 'react';
+import FireBaseFirestoreService from "../../services/Firebasefirestoreservice";
 
-    let orgData = [
-        {
-            imageUrl:"https://picsum.photos/400/400?rand=431",
-            type: "Donors",
-            count:256
-        },
-        {
-            imageUrl:"https://picsum.photos/400/400?rand=432",
-            type: "Donors",
-            count:256
-        },
-        {
-            imageUrl:"https://picsum.photos/400/400?rand=433",
-            type: "Donors",
-            count:256
-        },
-        {
-            imageUrl:"https://picsum.photos/400/400?rand=434",
-            type: "Donors",
-            count:256
-        }
-    ]
+
+export default function OrgDataCard(){
+    const [orgImpact, setOrgImpact] = useState([]);
+
+  useEffect(() => {
+    async function getOrgImpactData() {
+      try {
+        const data = await FireBaseFirestoreService.getDocumentsInArray("organization_profile");
+        setOrgImpact(data || []);
+        console.log(data)
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    getOrgImpactData();
+  }, []);
+
+    // let orgData = [
+    //     {
+    //         imageUrl:"https://picsum.photos/400/400?rand=431",
+    //         type: "Donors",
+    //         count:256
+    //     },
+    //     {
+    //         imageUrl:"https://picsum.photos/400/400?rand=432",
+    //         type: "Donors",
+    //         count:256
+    //     },
+    //     {
+    //         imageUrl:"https://picsum.photos/400/400?rand=433",
+    //         type: "Donors",
+    //         count:256
+    //     },
+    //     {
+    //         imageUrl:"https://picsum.photos/400/400?rand=434",
+    //         type: "Donors",
+    //         count:256
+    //     }
+    // ]
     return (
-        orgData.map((el) => <div className="card-wrapper">
+        <div className="charityInformation">
+        {orgImpact.map(profile => (
+            <div key={profile.id} className="card-wrapper">
+                <div className="card-1">
                     <div className="img-wrapper">
-                        <img src={el.imageUrl} alt="randompic"/>
+                        <img src="https://picsum.photos/200/200?rand=434" alt=""/>
                     </div>
-                    <p>
-                       {el.count}
-                    </p>
-                    <p>
-                        Total {el.type}
-                    </p>
-               </div>)
+                    <h2>{profile.impactDetails.founded_year}</h2>
+                    <p>Founded Year</p>
+                </div>
+
+                <div className="card-2">
+                    <div className="img-wrapper">
+                        <img src="https://picsum.photos/200/200?rand=434" alt=""/>
+                    </div>
+                    <h2>{profile.impactDetails.total_donors}</h2>
+                    <p>Total Donors</p>
+                </div>
+
+                <div className="card-3">
+                    <div className="img-wrapper">
+                        <img src="https://picsum.photos/200/200?rand=434" alt=""/>
+                    </div>
+                    <h2>{profile.impactDetails.kids}</h2>
+                    <p>Kids</p>
+                </div>
+
+                <div className="card-4">
+                    <div className="img-wrapper">
+                        <img src="https://picsum.photos/200/200?rand=434" alt=""/>
+                    </div>
+                    <h2>{profile.impactDetails.toys}</h2>
+                    <p>Donated Toys</p>
+                </div>
+            </div>
+        ))}
+
+    </div>
     )
 }
