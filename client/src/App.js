ae0ea00ed95c3f2dbf89d3e18d5042885ec46484
MagdelineJannet <janet.magdeline@gmail.com>
2023-07-12 19:19:46 -0700

    reverted changes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import DonorDonationProcessForm from './components/donor-donation-process-form';
-
 
 import Routing from "./routes/routing";
 function App() {
+  
+
   return (
     <div className="App">
-        {/* <Routing/> */}
-        <DonorDonationProcessForm/>
+        <Routing/>
     </div>
     
   );
