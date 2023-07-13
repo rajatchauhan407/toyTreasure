aa31741fe1e83a8fb6aba496d17e4758ec80f4c5
MagdelineJannet <janet.magdeline@gmail.com>
2023-07-12 19:17:52 -0700

    getting user details and delivery method for donation process

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import DonorDonationProcessForm from './components/donor-donation-process-form';
 
 
 import Routing from "./routes/routing";
 function App() {
   return (
     <div className="App">
-        <Routing/>
+        {/* <Routing/> */}
+        <DonorDonationProcessForm/>
     </div>
     
   );
--- client/src/components/donor-donation-process-form/index.js
+++ client/src/components/donor-donation-process-form/index.js
@@ -1,14 +1,34 @@
+import { useContext } from "react";
 import "./index.scss";
 import { Link } from "react-router-dom";
+import AuthContext from "../../services/auth-context";
+import { useState,useEffect } from 'react';
+import FireBaseFirestoreService from '../../services/Firebasefirestoreservice';
 
 export default function DonorDonationProcessForm(props)
 {
-    // function handleAccept(){
-    //     props.onAccept(true);
-    // }
-   
-    const donorDetails_tt81 = [
-        {donorName:"Greg Thomas",donorPhone:"+1 673 564 7823"}       
+    function handleAccept(){
+        props.onAccept(true);
+    }
+    const [orgDeliveryDetails, setOrgDeliveryDetails] = useState([]); 
+    async function getOrgDeliveryDetails()
+    {
+
+        try {
+            const data = await FireBaseFirestoreService.getDocumentsInArray("organization_profile");
+            setOrgDeliveryDetails(data || []);
+            console.log("delivery details>>> "+orgDeliveryDetails.extraDetails)
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+       
+    }
+    useEffect(()=>{
+        getOrgDeliveryDetails();
+    },[]);
+   let authCtx=useContext(AuthContext);
+   const donorDetails_tt81 = [
+        {donorName:authCtx.displayName,donorEmail:authCtx.email}       
     ]
     return( 
         <form className="tt-81-donationForm">   
@@ -20,7 +40,7 @@ export default function DonorDonationProcessForm(props)
                             <Link to="#">Edit</Link>
                         </div>
                         <p>Name: {donorDetails_tt81[0].donorName}</p>
-                        <p>Phone: {donorDetails_tt81[0].donorPhone}</p>
+                        <p>Email: {donorDetails_tt81[0].donorEmail}</p>
                     </div>
                     <div className="tt-81-delivery-method"> 
                         <h3>Delivery Method</h3>
@@ -40,17 +60,17 @@ export default function DonorDonationProcessForm(props)
                     </div>
                     <div className="tt-81-donation-instruction"> 
                         <h3>Donation Instruction</h3> 
-                        <p><i class="fa-solid fa-pump-soap"></i> Clean, wash (if applicable) and sanitize the toys.</p>
-                        <p><i class="fa-solid fa-ban-smoking"></i> We only accept toys from smoke and pet free homes.</p>
+                        <p><i className="fa-solid fa-pump-soap"></i> Clean, wash (if applicable) and sanitize the toys.</p>
+                        <p><i className="fa-solid fa-ban-smoking"></i> We only accept toys from smoke and pet free homes.</p>
                         <p>Please visit toystreasure.ca/donation-guideline for more info.</p>
                     </div>
                 
                     <div className="tt-81-checkboxes"> 
-                        <label class="tt-81-remind-me-label">
+                        <label className="tt-81-remind-me-label">
                             <input type="checkbox" id="tt-81-remind-me"  name="tt-81-remind-me"  />
                             Remind me to drop-off/pick-up donation via Text Message.
                         </label>
-                        <label class="tt-81-confirm-details-label">
+                        <label className="tt-81-confirm-details-label">
                             <input type="checkbox" id="tt-81-confirm-details"  name="tt-81-confirm-details"  />
                             I hereby confirm that all the details are correct and follow guidelines for donation.
                         </label>
