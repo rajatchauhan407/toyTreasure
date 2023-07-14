89d4e686ee0cf68f3ac26d0925e5cbeeb6f4aaf6
Raha <r.emami98@gmail.com>
2023-07-13 19:38:46 -0700

    Login Sign up Compeleted

--- client/src/components/loginsignupform/index.js
+++ client/src/components/loginsignupform/index.js
@@ -1,4 +1,6 @@
 import "./index.scss";
+import donorIcon from './donorIcon.png';
+import orgIcon from './organizationIcon.png';
 import logo from "./appLogo.svg"
 import { Link } from "react-router-dom";
 
@@ -15,15 +17,29 @@ function LoginSignupForm(props) {
         <img className="site-logo" src={logo} alt={"siteLogo"}/>
     </div>
 
-    <h1>Start Your Journey of Giving</h1>
+    <h3>Start Your Journey of Giving</h3>
 
     <p>Take the first step toward making a difference by choosing how you want to join us.</p>
 
-    <button className="createAccountDonor" onClick={handleSignupDonor}><h2>Create Account as Donor</h2>
-    <p>Start making a difference by donating toys to organizations.</p></button> 
+    <button className="createAccountDonor" onClick={handleSignupDonor}>
 
-    <button className="createAccountOrganization" onClick={handleSignupOrg}><h2>Create Account as Organization</h2>
-    <p>Recieve toy donations and support your cause.</p></button>    
+    <img className="donorIcon" src={donorIcon} alt={"Donor Icon"}/>
+
+    <div className="gridIconLogin">
+        <h5>Create Account as Donor</h5>
+         <p>Start making a difference by donating toys.</p>
+    </div>
+    </button> 
+
+    <button className="createAccountOrganization" onClick={handleSignupOrg}>
+        
+        <img className="orgIcon" src={orgIcon} alt={"Organization Icon"}/>
+
+        <div className="gridIconLogin">
+            <h5>Create Account as Organization</h5>
+            <p>Recieve toy donations and support your cause.</p>
+        </div>
+    </button>    
 
 <p>Already have an account? <Link to="/login">Sign In!</Link></p>
 </div>
