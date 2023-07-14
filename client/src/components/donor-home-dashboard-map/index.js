793e7d2abc05947b10d32728d35a7ae226ce4235
cindycharvet <cindycharvetweb@gmail.com>
2023-07-14 05:23:22 -0700

    map style done

--- client/src/components/donor-home-dashboard-map/index.js
+++ client/src/components/donor-home-dashboard-map/index.js
@@ -10,30 +10,34 @@ import map5 from './map5.png';
 export default function DonorHomeDashboardMap(){
     return(
     <div className="donorHomeDashboardMap">
-            
+
+        <div className='mapPoints'>
             <Link className='donorMapImage1' to="/map">
              <img src={map1} alt="toy1" />
             </Link>
 
-            <Link className='donorMapImage2' href="/map">
+            <Link className='donorMapImage2' to="/map">
              <img src={map2} alt="toy2" />
             </Link>
 
-            <Link className='donorMapImage3' href="/map">
+            <Link className='donorMapImage3' to="/map">
              <img src={map3} alt="toy3" />
             </Link>
 
-            <Link className='donorMapImage4' href="/map">
+            <Link className='donorMapImage4' to="/map">
              <img src={map4} alt="toy4" />
             </Link>
 
-            <Link className='donorMapImage5' href="/map">
+            <Link className='donorMapImage5' to="/map">
              <img src={map5} alt="toy5" />
             </Link>
+        </div>
 
+        <div className='mapInfo'>
             <h2>Explore nearby charity centers and be the reason for someones happiness.</h2>
             
-            <button><Link to="/map">Donate now to the nearest charity</Link></button>
+            <Link to="/map"><button>Donate now to the nearest charity</button></Link>
+        </div>
 
     </div>
     )
