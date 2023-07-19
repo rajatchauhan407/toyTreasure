c6257e4c84543ef0c333d37bbd766f344fc478e3
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-19 02:09:41 -0700

    almost done

--- client/src/components/donor-home-dashboard-pending-donation/index.js
+++ client/src/components/donor-home-dashboard-pending-donation/index.js
@@ -1,14 +1,124 @@
 import './index.scss';
+import { Html5QrcodeScanner } from 'html5-qrcode'
 import { useEffect, useState, useRef, useContext } from 'react';
 import FireBaseFirestoreService from "../../services/Firebasefirestoreservice";
 import AuthContext from '../../services/auth-context';
-import { BarcodeReader } from '@zxing/library';
+
+
+const qrcodeRegionId = "html5qr-code-full-region";
+
+// Creates the configuration object for Html5QrcodeScanner.
+const createConfig = (props) => {
+    let config = {};
+    if (props.fps) {
+        config.fps = props.fps;
+    }
+    if (props.qrbox) {
+        config.qrbox = props.qrbox;
+    }
+    if (props.aspectRatio) {
+        config.aspectRatio = props.aspectRatio;
+    }
+    if (props.disableFlip !== undefined) {
+        config.disableFlip = props.disableFlip;
+    }
+    return config;
+};
+
+const Html5QrcodePlugin = (props) => {
+
+    useEffect(() => {
+        // when component mounts
+        const config = createConfig(props);
+        const verbose = props.verbose === true;
+        // Suceess callback is required.
+        if (!(props.qrCodeSuccessCallback)) {
+            throw "qrCodeSuccessCallback is required callback.";
+        }
+        const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
+        html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
+
+        // cleanup function when component will unmount
+        return () => {
+            html5QrcodeScanner.clear().catch(error => {
+                console.error("Failed to clear html5QrcodeScanner. ", error);
+            });
+        };
+    }, []);
+
+    return (
+        <div id={qrcodeRegionId} />
+    );
+};
+
+
+
+function filterResults (results) {
+  let filteredResults = [];
+  for (var i = 0; i < results.length; ++i) {
+      if (i === 0) {
+          filteredResults.push(results[i]);
+          continue;
+      }
+
+      if (results[i].decodedText !== results[i - 1].decodedText) {
+          filteredResults.push(results[i]);
+      }
+  }
+  return filteredResults;
+}
+
+const ResultContainerTable = ({ data }) => {
+  const results = filterResults(data);
+  return (
+      <table className={'Qrcode-result-table'}>
+          <thead>
+              <tr>
+                  <td>#</td>
+                  <td>Decoded Text</td>
+                  <td>Format</td>
+              </tr>
+          </thead>
+          <tbody>
+              {
+                  results.map((result, i) => {
+                      console.log(result);
+                      return (<tr key={i}>
+                          <td>{i}</td>
+                          <td>{result.decodedText}</td>
+                          <td>{result.result.format.formatName}</td>
+                      </tr>);
+                  })
+              }
+          </tbody>
+      </table>
+  );
+};
+
+const ResultContainerPlugin = (props) => {
+  const results = filterResults(props.results);
+  return (
+      <div className='Result-container'>
+          <div className='Result-header'>Scanned results ({results.length})</div>
+          <div className='Result-section'>
+              <ResultContainerTable data={results} />
+          </div>
+      </div>
+  );
+};
+
+
 export default function DashboardPendingDonation(props) {
   const [donorPendingDonation, setPendingDonation] = useState([]);
   const [isCameraOpen, setIsCameraOpen] = useState(false);
   const videoRef = useRef(null);
+
   const authCtx = useContext(AuthContext);
-  const [scannedData, setScannedData] = useState('');
+  const [decodedResults, setDecodedResults] = useState([]);
+    const onNewScanResult = (decodedText, decodedResult) => {
+        console.log("App [result]", decodedResult);
+        setDecodedResults(prev => [...prev, decodedResult]);
+    };
 
   useEffect(() => {
     console.log(props.donations);
@@ -38,43 +148,27 @@ export default function DashboardPendingDonation(props) {
    
   // };
 
-  const handleScanQRCode = async () => {
-    try{
-      const codeReader = new BarcodeReader();
-      const videoElement = videoRef.current;
-      const constraints = {video:{facingMode:"environment"}};
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      videoElement.srcObject = stream;
-      const result = await codeReader.decodeOnceFromVideoDevice(undefined, videoElement)
-      setScannedData(result.getText())
-        
-    } catch (err) {
-      console.error('Error accessing camera:', err);
-    }
-    
-
-  };
-
   return (
     <div className='tt-72-DashboardPendingDonationWrapper'>
       <div className="tt-72-DashboardPendingDonation">
         <h3>You have a Pending Donation</h3>
-        {donorPendingDonation.map((donation, index) => (
-          <div key={index}>
-            <h3>{donation.org_name}</h3>
-            {/* <p>{formatDate(donation.user_donation_date)}</p> */}
-          </div>
-        ))}
-        <video ref={videoRef} width="640" height="480" />
+        
+        {/* <video ref={videoRef} width="640" height="480" />
         <input
           type="button"
           value="Scan QR Code"
           id="tt72scanQR"
           className="tt72scanQR"
           onClick={handleScanQRCode}
-        />
+        /> */}
         <div>
-          <p>{scannedData}</p>
+        <Html5QrcodePlugin
+                    fps={10}
+                    qrbox={250}
+                    disableFlip={false}
+                    qrCodeSuccessCallback={onNewScanResult}
+                />
+        {/* <ResultContainerPlugin results={decodedResults} /> */}
     </div>
         {/* {isCameraOpen && (
           <div>
