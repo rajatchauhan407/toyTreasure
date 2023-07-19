272d273b8c37b4660657c8b917c18263905823d0
cindycharvet <cindycharvetweb@gmail.com>
2023-07-18 18:16:01 -0700

    charity back arrow

--- client/src/components/donor-charity-profile-information/index.js
+++ client/src/components/donor-charity-profile-information/index.js
@@ -1,4 +1,5 @@
 import "./index.scss";
+import arrowBack from './../../pages/map/back_arrow.png';
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import FireBaseFirestoreService from "../../services/Firebasefirestoreservice";
@@ -17,10 +18,16 @@ export default function DonorCharityProfileInformation({info,logo, main}){
     <div className="charityInformation">
         
             <div key={orgCharityProfile.id} className="profileinformation">
+              <div className="link">
+                <Link to="/map"><button className="back"><img src={arrowBack} alt="back-arrow"/></button></Link>
+              </div>
+
+              <div className="information">
                 <img className="charity-logo" src={logo} alt="logo-charity" />
                 <h2>{orgCharityProfile.org_name}</h2>
                 <p>{orgCharityProfile.org_description}</p>
                 <button><Link to={`/donation/toys/${id}`}>Donate Now</Link></button>
+              </div>
             </div>
         
 
