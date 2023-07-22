2b188dd19a3c59388a8a7391f067f37171f1a5b9
cindycharvet <cindycharvetweb@gmail.com>
2023-07-21 17:58:59 -0700

    changing the rem to px in typography

--- client/src/components/home-dashboard-stories/index.js
+++ client/src/components/home-dashboard-stories/index.js
@@ -3,8 +3,8 @@ export default function HomeDashBoardStories(){
     return (
         <div className="tt-50-container">
                 <div className="tt-50-main">  
-                <h1> What are they talking about us?</h1>
-                <p className="body">Sharing smiles is local charity based on local charities, businesses and concerned citizenry whose sole focus as an organization.</p>
+                <h1>Toyful tales about our giving mission.</h1>
+                <p className="body">Toys Treasure is a dedicated toy donation app that aims to bring smiles and happiness to children by connecting donors with those in need.</p>
                 </div>
                 <div className="tt-50-container-item">
                 <p>Sharing smiles is local charity based on local charities, businesses and concerned citizenry whose sole focus as an organization.</p>
