388c9b6f37e70a27049d6e2e187cfca3679ae9e9
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-26 06:35:38 -0700

    mobile header does not close on start but closes on click. almost both the headers fixed

--- client/src/components/org-header-mobile/index.js
+++ client/src/components/org-header-mobile/index.js
@@ -2,9 +2,12 @@ import "./index.scss";
 import logo from "./appLogo.svg"
 import { Link, useNavigate } from "react-router-dom";
 import FireBaseAuthService from "../../services/FirebaseAuthService";
+import { useEffect } from "react";
 export default function OrgHeaderMobile(props)
 {
-    
+    useEffect(()=>{
+        toggleMenuOrg();
+    },[])
     function toggleMenuOrg()
     {
         document.body.classList.toggle('tt-223-menu-active');
@@ -22,9 +25,9 @@ export default function OrgHeaderMobile(props)
             </div>                  
             <nav className="tt-223-main-menu">
                 <ul>
-                    <li><Link to="/organization/profile"><i className="fa fa-user-o" aria-hidden="true"></i> Profile</Link></li>
-                    <li><Link to="/organization/dashboard"><i className="fa-sharp fa-solid fa-chart-line"></i> Dashboard</Link></li>               
-                    <li><Link to="/organization/verification"><i className="fa-brands fa-wpforms"></i> Verification</Link></li>
+                    <li ><Link onClick={toggleMenuOrg}  to="/organization/profile"><i className="fa fa-user-o" aria-hidden="true"></i> Profile</Link></li>
+                    <li ><Link onClick={toggleMenuOrg} to="/organization/dashboard"><i className="fa-sharp fa-solid fa-chart-line"></i> Dashboard</Link></li>               
+                    <li ><Link onClick={toggleMenuOrg} to="/organization/verification"><i className="fa-brands fa-wpforms"></i> Verification</Link></li>
                     <li><Link onClick={handleLogout} to="#"><i className="fa fa-sign-out" aria-hidden="true"></i> Logout</Link></li>      
                 </ul>
             </nav>                      
