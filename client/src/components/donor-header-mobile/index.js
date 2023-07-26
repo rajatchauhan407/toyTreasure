ed2378ceb2513d6dfd22d4534bb52e2e219de7d7
MagdelineJannet <janet.magdeline@gmail.com>
2023-07-25 17:05:38 -0700

    responsive header added for donor mobile

--- client/src/components/donor-header-mobile/index.js
+++ client/src/components/donor-header-mobile/index.js
@@ -1,20 +1,26 @@
 import "./index.scss";
 import logo from "./appLogo.svg"
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
+import FireBaseAuthService from "../../services/FirebaseAuthService";
 export default function DonorHeaderMobile(props)
 {
+    const navigate = useNavigate();
+    async function handleLogout(){
+        await FireBaseAuthService.logoutUser();
+        navigate('/login');
+    }
     return(
         <div className="tt-22-donorHeaderWrapper">
             <div class="tt-22-donor-image-wrapper">
-                <img className="tt-22-site-logo" src={logo} alt={"siteLogo"}/>                              
-                <i class="far fa-times-circle"></i>  
+            <Link to="/home"><img className="tt-22-site-logo" src={logo} alt={"siteLogo"}/></Link>                              
+                <button><i class="far fa-times-circle"></i></button>  
             </div>                  
             <nav className="tt-22-main-menu">
                 <ul>
-                    <li><Link to="#"><i className="fa fa-home"></i> Home</Link></li>
-                    <li><Link to="#"><i className="fas fa-compass"></i> Find Charities</Link></li>               
-                    <li><Link to="#"><i className="far fa-star"></i> Rewards</Link></li>
-                    <li><Link to="#" className="tt-22-logout"><i class="fas fa-sign-out"></i> Logout</Link></li>     
+                    <li><Link to="/home"><i className="fa fa-home"></i> Home</Link></li>
+                    <li><Link to="/map"><i className="fas fa-compass"></i> Find Charities</Link></li>               
+                    <li><Link to="/rewards"><i className="far fa-star"></i> Rewards</Link></li>
+                    <li><Link onClick={handleLogout} className="tt-22-logout"><i class="fas fa-sign-out"></i> Logout</Link></li>     
                 </ul>
             </nav>                      
         </div>
