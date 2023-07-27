9de3a0d22c60a018cf320013cde2cc90f64ab2ee
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-07-27 00:43:18 -0700

    pwa done partially

---
+++ client/src/components/no-internet/index.js
@@ -0,0 +1,12 @@
+// NoInternetComponent.js
+import React from 'react';
+
+const NoInternetComponent = () => {
+  return (
+    <div style={{ background: 'red', color: 'white', padding: '10px', textAlign: 'center' }}>
+      No internet connection. Please check your network settings.
+    </div>
+  );
+};
+
+export default NoInternetComponent;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import { AuthContextProvider } from './services/auth-context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 root.render(
     <BrowserRouter>
@@ -16,7 +18,7 @@ root.render(
     </BrowserRouter>
   
 );
-
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
