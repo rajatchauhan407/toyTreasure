2e81daa42bc5bbcbf44ae6d3d3bc84ffc8ea9cc8
cindycharvet <cindycharvetweb@gmail.com>
2023-08-01 22:33:19 -0700

    improvements in the active QR section of verification

--- client/src/components/org-verification-qr-card/index.js
+++ client/src/components/org-verification-qr-card/index.js
@@ -6,10 +6,10 @@ export default function OrgVerificationQRCard(props)
     return(  
         <div className="OrgVerQRCardWrapper">                            
             <div className="org-ver-qr-card">
-               <h2>QR Code</h2>
+               <h4>QR Code</h4>
                 <p>Show below QR Code to the donor to redeem points.</p>
                 <div className="orgVerQRMessage">
-                    <h3>QR Code for the donors will be generated after verification</h3>
+                    <h3>QR Code for donor will be generated after verification</h3>
                 </div>
             </div>              
         </div>    
