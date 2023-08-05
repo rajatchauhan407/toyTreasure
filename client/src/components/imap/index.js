9447d1c1534b583cd248df730ff10f9ce507ff88
Rajat Chauhan <rajatchauhan407@gmail.com>
2023-08-05 05:21:14 -0700

    improvement in maps

--- client/src/components/imap/index.js
+++ client/src/components/imap/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import './index.scss';
 import { GoogleMap, LoadScript,Marker} from '@react-google-maps/api';
 import { useEffect,useState } from 'react';
@@ -6,6 +7,9 @@ const InteractiveMap = ()=>{
     const[mapsApiKey, setMapsApiKey] = useState({});
     const [loading, setLoading] = useState(true);
     const [location,setLocation] = useState(null);
+    // const [isMarkerClicked, setIsMarkerClicked] = useState(false);
+    const [zoomMap,setZoomMap] = useState(10);
+    const [center,setCenter] = useState(null);
     useEffect(() => {
       async function getApi(){
         try{
@@ -152,12 +156,15 @@ const InteractiveMap = ()=>{
     
     
     
-    
-    
     // const markerIcon = {
     //   url: 'https://picsum.photos/100/100/random=239',
     //   scaledSize: {width:40,height:40}
     // };
+    function handleMarkerClick(center){
+      setCenter(center);  
+      setZoomMap(14);
+      console.log("hello");
+    }
   useEffect(()=>{
       if(navigator.geolocation){
         console.log(navigator.geolocation)
@@ -168,25 +175,22 @@ const InteractiveMap = ()=>{
                       latitude:position.coords.latitude,
                       longitude:position.coords.longitude
                     })
-                  }
+                  }     
         )
       };
       console.log(location);
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+  },[]);
     
     const containerStyle = {
         width: '100%',
         height: '100vh'
       };
 
-      const center = {
-        lat: location?.latitude ,
-        lng: location?.longitude
-      };
       if (loading) {
         return <LoaderToysTreasure />;
       }
+      
     return <>
     {mapsApiKey && (<LoadScript
       googleMapsApiKey={mapsApiKey.api}
@@ -194,18 +198,22 @@ const InteractiveMap = ()=>{
     <GoogleMap
         options={{ styles: mapStyles }}
         mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
+        center={ center || {lat:location?.latitude, lng:location?.longitude}}
+        zoom={zoomMap}
     >
         { /* Child components, such as markers, info windows, etc. */ }
        <Marker 
-        position={center} 
+        position={{lat:location?.latitude,lng:location?.longitude}} 
         title='Your Location'
         icon={{
               url:'https://firebasestorage.googleapis.com/v0/b/toystreasure-50c4d.appspot.com/o/icons%2Fmap-pin-user.svg?alt=media&token=4cc120c4-9f9b-4fbf-85e1-05b014986f0d' ,
               // scaledSize: new window.google.maps.Size(30, 30)
               
             }}
+            onClick={()=>handleMarkerClick({
+        lat: location?.latitude ,
+        lng: location?.longitude
+      })}
         // icon={markerIcon}
         />
         <Marker
@@ -218,6 +226,10 @@ const InteractiveMap = ()=>{
               url: 'https://firebasestorage.googleapis.com/v0/b/toystreasure-50c4d.appspot.com/o/icons%2Fmap-pin-default.svg?alt=media&token=39dab852-da50-4090-819b-cb35c20df43e'
               // scaledSize: new window.google.maps.Size(30, 30)
             }}
+            onClick={()=>{handleMarkerClick({
+            lat:49.281992646679946, 
+            lng:-123.08303057488982
+          })}}
         />
         <Marker
           title='Charity'
@@ -229,7 +241,41 @@ const InteractiveMap = ()=>{
               url: 'https://firebasestorage.googleapis.com/v0/b/toystreasure-50c4d.appspot.com/o/icons%2Fmap-pin-default.svg?alt=media&token=39dab852-da50-4090-819b-cb35c20df43e'
               // scaledSize: new window.google.maps.Size(30, 30)
             }}
+            onClick={()=>{handleMarkerClick({
+            lat:49.23639116081492,
+            lng: -123.04438619999999
+          })}}
         />
+        <Marker
+          title='Charity'
+          position={{
+          lat:49.263301999410416, 
+          lng:-123.12549340178631
+          }}
+          icon={{
+              url: 'https://firebasestorage.googleapis.com/v0/b/toystreasure-50c4d.appspot.com/o/icons%2Fmap-pin-default.svg?alt=media&token=39dab852-da50-4090-819b-cb35c20df43e'
+              // scaledSize: new window.google.maps.Size(30, 30)
+            }}
+            onClick={()=>handleMarkerClick({
+          lat:49.263301999410416, 
+          lng:-123.12549340178631
+          })}
+          />
+          <Marker
+          title='Charity'
+          position={{
+          lat:49.26268780421018,  
+          lng:-123.16888030363859,
+          }}
+          icon={{
+              url: 'https://firebasestorage.googleapis.com/v0/b/toystreasure-50c4d.appspot.com/o/icons%2Fmap-pin-default.svg?alt=media&token=39dab852-da50-4090-819b-cb35c20df43e'
+              // scaledSize: new window.google.maps.Size(30, 30)
+            }}
+            onClick={()=>{handleMarkerClick({
+          lat:49.26268780421018,  
+          lng:-123.16888030363859,
+          })}}
+          />
       </GoogleMap>
     </LoadScript>)}
     </>
